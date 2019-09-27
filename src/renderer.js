540ada99e6d2dbecb5176e5d1afcfc77c4133039
Harshit Srivastava <hs3500@nyu.edu>
2019-09-27 11:45:50 -0700

    fixed preview file

--- src/renderer.js
+++ src/renderer.js
@@ -175,8 +175,6 @@ ipcRenderer.on('selected-saveorganizationfile', (event, path) => {
   var jsonvect = tableToJsonWithDescription(tableNotOrganized)
   var jsonpath = jsonvect[0]
   var jsondescription = jsonvect[1]
-  console.log(jsonpath)
-  console.log(jsondescription)
   document.getElementById("save-file-organization-status").innerHTML = "";
   // Call python to save
   if (path != null){
@@ -199,16 +197,13 @@ selectUploadFileOrganizationBtn.addEventListener('click', (event) => {
   ipcRenderer.send('open-file-dialog-uploadorganization')
 })
 ipcRenderer.on('selected-uploadorganization', (event, path) => {
-  console.log(path)
   document.getElementById("upload-file-organization-status").innerHTML = "";
   var headernames = sparcFolderNames.slice()
   headernames.push("main")
   var lennames =  headernames.length
   for (var i = 0; i < lennames; i++) {
-  	console.log(headernames[i])
   	headernames.push(headernames[i] + "_description")
   }
-  console.log(headernames)
   client.invoke("apiUploadFileOrganization", path[0], headernames, (error, res) => {
         if(error) {
           console.log(error)
@@ -227,7 +222,6 @@ selectPreviewBtn.addEventListener('click', () => {
   document.getElementById("preview-organization-status").innerHTML = ""
   var jsonvect = tableToJsonWithDescription(tableNotOrganized)
   var jsonpath = jsonvect[0]
-  console.log(jsonpath)
   client.invoke("apiPreviewFileOrganization", jsonpath, (error, res) => {
       if(error) {
         console.log(error)
@@ -242,7 +236,7 @@ selectPreviewBtn.addEventListener('click', () => {
 
 // Action when user click on Preview file organization button
 deletePreviewBtn.addEventListener('click', () => {
-  // document.getElementById("delete-preview-organization-status").innerHTML = ""
+  document.getElementById("preview-organization-status").innerHTML = ""
   client.invoke("apiDeletePreviewFileOrganization", (error, res) => {
       if(error) {
         console.log(error)
@@ -357,8 +351,6 @@ curateDatasetBtn2.addEventListener('click', () => {
   }
 
   if (alreadyorganizedstatus.checked) {
-    console.log(alreadyorganizedstatus.checked)
-    console.log(pathdataset.value)
     if (fs.existsSync(pathdataset.value)) {
       jsonvar = organizedFolderToJson(pathdataset.value)
     } else {
@@ -371,7 +363,6 @@ curateDatasetBtn2.addEventListener('click', () => {
   }
 
   console.log('log')
-  console.log(jsonvar)
 
   var metadatafiles = []
 
@@ -424,8 +415,6 @@ curateDatasetBtn2.addEventListener('click', () => {
   }
 
   jsonvar['metadata'] = metadatafiles
-
-  console.log(jsonvar)
 
   progressinfo.value = ''
   var completionstatus = 'Solving'
@@ -655,8 +644,6 @@ function checkFolderStruture(pathDatasetFolder){
       folders.push(filename)
     }
   }
-  console.log(folders)
-  console.log(sparcFolderNames)
   if (folders.length != sparcFolderNames.length)
         return false
   var foldersorted = folders.sort()
@@ -717,8 +704,6 @@ function insertFileToTable(table, path){
   var i
   let SPARCfolder = document.querySelector('#SPARCfolderlist').value
   var rowcount = document.getElementById(SPARCfolder).rowIndex
-  console.log(SPARCfolder)
-  console.log(rowcount)
   for (i = 0; i < path.length; i++) {
     tableNotOrganizedcount = tableNotOrganizedcount + 1
     var table_len=tableNotOrganizedcount
@@ -776,7 +761,6 @@ function tableToJsonWithDescription(table){
   }
   jsonvar[keyval] = pathlist
   jsonvardescription[keyval+ "_description"] = descriptionlist
-  console.log(jsonvar)
 
   return [jsonvar, jsonvardescription]
 }
@@ -829,11 +813,7 @@ function jsonToTableWithDescription(table, jsonvar){
 function clearTable(table){
   var keyvect = sparcFolderNames.slice()
   keyvect.push("main")
-  console.log(table.rows.length)
-  console.log(keyvect.length)
   while (table.rows.length > keyvect.length){
-    console.log(table.rows.length)
-    console.log(keyvect.length)
     var keyrow = []
     for (var j = 0; j < keyvect.length; j++) {
       var SPARCfolderid = keyvect[j]
@@ -843,7 +823,6 @@ function clearTable(table){
       var rowcount = document.getElementById(SPARCfolderid).rowIndex
       keyrow.push(rowcount)
     }
-    console.log(keyrow)
     for (var i = 0; i < table.rows.length; i++) {
       if (keyrow.includes(i) === false){
         table.deleteRow(i)
