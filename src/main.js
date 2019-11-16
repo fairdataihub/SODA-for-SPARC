807629b3862ecfdf8a3fbf134d9937d7fdbef7be
bvhpatel <8172375+bvhpatel@users.noreply.github.com>
2019-11-15 18:58:52 -0800

    design changed

--- src/main.js
+++ src/main.js
@@ -115,12 +115,6 @@ function initialize () {
 }
 
 // Make this app a single instance app.
-//
-// The main window will be restored and focused instead of a second window
-// opened when a person attempts to launch a second instance.
-//
-// Returns true if the current version of the app should quit instead of
-// launching.
 const gotTheLock = app.requestSingleInstanceLock()
 
 function makeSingleInstance () {
