0c388118dda0bb2d1d74623ebb38bc51f77f7897
bvhpatel <8172375+bvhpatel@users.noreply.github.com>
2019-11-18 10:44:26 -0800

    Table moved above all buttons in Specify Files

--- src/renderer.js
+++ src/renderer.js
@@ -1048,7 +1048,7 @@ function jsonToTableOrganized(table, jsonvar){
       tableOrganizedCount = tableOrganizedCount + 1
       var rownum = rowcount + i + 1
       var table_len = tableOrganizedCount
-      var row = table.insertRow(rownum).outerHTML="<tr id='row-org"+table_len+"'style='color: #000000;'><td id='name_row_org"+table_len+"'>"+ pathlist[i] +"</td> <td id='description_row_org"+table_len+"'>"+ "" +"</td> <td><input type='button' id='edit_button_org"+table_len+"' value='Edit description' class='edit' onclick='edit_row_org("+table_len+")'> <input type='button' id='save_button_org"+table_len+"' value='Save description' class='save' onclick='save_row_org("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row_org("+table_len+")'> </td></tr>";
+      var row = table.insertRow(rownum).outerHTML="<tr id='row-org"+table_len+"'style='color: #000000;'><td id='name_row_org"+table_len+"'>"+ pathlist[i] +"</td> <td id='description_row_org"+table_len+"'>"+ "" +"</td> <td><input type='button' id='edit_button_org"+table_len+"' value='Edit description' class='edit' onclick='edit_row_org("+table_len+")'> <input type='button' id='save_button_org"+table_len+"' value='Save description' class='save' onclick='save_row_org("+table_len+")'> </td></tr>";
     }
   }
   return table
