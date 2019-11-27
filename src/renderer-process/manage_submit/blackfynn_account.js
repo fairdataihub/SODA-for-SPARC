28677d5e0883ff3353aa4f2ac87827e30ef69aa2
Harshit Srivastava <hs3500@nyu.edu>
2019-11-26 17:27:59 -0800

    added forms, changed to csv, removed desc

--- src/renderer-process/manage_submit/blackfynn_account.js
+++ src/renderer-process/manage_submit/blackfynn_account.js
@@ -17,3 +17,100 @@ document.querySelector('#addAccount').addEventListener('click', () => {
 document.querySelector('#selectAccount').addEventListener('click', () => {
     showSelectAccount()
 })
+
+//
+// Blackfynn Metadata
+//
+function showAddEditSubtitle(){
+  document.getElementById('div-add-edit-subtitle').style.display ='block';
+  document.getElementById('div-add-edit-description').style.display ='none';
+  document.getElementById('div-add-edit-banner').style.display ='none';
+  document.getElementById('div-add-edit-license').style.display ='none';
+}
+function showAddEditDescription(){
+  document.getElementById('div-add-edit-subtitle').style.display ='none';
+  document.getElementById('div-add-edit-description').style.display ='block';
+  document.getElementById('div-add-edit-banner').style.display ='none';
+  document.getElementById('div-add-edit-license').style.display ='none';
+}
+function showAddEditBanner(){
+  document.getElementById('div-add-edit-subtitle').style.display ='none';
+  document.getElementById('div-add-edit-description').style.display ='none';
+  document.getElementById('div-add-edit-banner').style.display ='block';
+  document.getElementById('div-add-edit-license').style.display ='none';
+}
+function showAddEditLicense(){
+  document.getElementById('div-add-edit-subtitle').style.display ='none';
+  document.getElementById('div-add-edit-description').style.display ='none';
+  document.getElementById('div-add-edit-banner').style.display ='none';
+  document.getElementById('div-add-edit-license').style.display ='block';
+}
+
+document.querySelector('#addEditSubtitle').addEventListener('click', () => {
+    showAddEditSubtitle()
+})
+document.querySelector('#addEditDescription').addEventListener('click', () => {
+    showAddEditDescription()
+})
+document.querySelector('#addEditBanner').addEventListener('click', () => {
+    showAddEditBanner()
+})
+document.querySelector('#addEditLicense').addEventListener('click', () => {
+    showAddEditLicense()
+})
+//
+// Manage permissions
+//
+function showPIOwner(){
+  document.getElementById('div-bf-pi-owner').style.display ='block';
+  document.getElementById('div-bf-share-curation-team').style.display ='none';
+  document.getElementById('div-bf-user-permission').style.display ='none';
+  document.getElementById('div-bf-team-permission').style.display ='none';
+  document.getElementById("para-dataset-permission-status-pi").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-curation-team").innerHTML = ""
+  document.getElementById("para-dataset-permission-status").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-team").innerHTML = ""
+}
+function showShareCurationTeam(){
+  document.getElementById('div-bf-pi-owner').style.display ='none';
+  document.getElementById('div-bf-share-curation-team').style.display ='block';
+  document.getElementById('div-bf-user-permission').style.display ='none';
+  document.getElementById('div-bf-team-permission').style.display ='none';
+  document.getElementById("para-dataset-permission-status-pi").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-curation-team").innerHTML = ""
+  document.getElementById("para-dataset-permission-status").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-team").innerHTML = ""
+}
+function showUserPermission(){
+  document.getElementById('div-bf-pi-owner').style.display ='none';
+  document.getElementById('div-bf-share-curation-team').style.display ='none';
+  document.getElementById('div-bf-user-permission').style.display ='block';
+  document.getElementById('div-bf-team-permission').style.display ='none';
+  document.getElementById("para-dataset-permission-status-pi").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-curation-team").innerHTML = ""
+  document.getElementById("para-dataset-permission-status").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-team").innerHTML = ""
+}
+function showTeamPermission(){
+  document.getElementById('div-bf-pi-owner').style.display ='none';
+  document.getElementById('div-bf-share-curation-team').style.display ='none';
+  document.getElementById('div-bf-user-permission').style.display ='none';
+  document.getElementById('div-bf-team-permission').style.display ='block';
+  document.getElementById("para-dataset-permission-status-pi").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-curation-team").innerHTML = ""
+  document.getElementById("para-dataset-permission-status").innerHTML = ""
+  document.getElementById("para-dataset-permission-status-team").innerHTML = ""
+}
+
+document.querySelector('#pi-owner').addEventListener('click', () => {
+    showPIOwner()
+})
+document.querySelector('#curation-team').addEventListener('click', () => {
+    showShareCurationTeam()
+})
+document.querySelector('#user-permission').addEventListener('click', () => {
+    showUserPermission()
+})
+document.querySelector('#team-permission').addEventListener('click', () => {
+    showTeamPermission()
+})
--- src/renderer.js
+++ src/renderer.js
@@ -457,7 +457,7 @@ curateDatasetBtn.addEventListener('click', () => {
     metadatafiles.push(pathSubmission)
   } else if (newSubmissionStatus.checked === true) {
     submissionStatus = true
-    pathSubmission = path.join(__dirname, 'file_templates', 'submission.csv')
+    pathSubmission = path.join(__dirname, 'file_templates', 'submission.xlsx')
     metadatafiles.push(pathSubmission)
   } else {
     submissionStatus = false
@@ -469,7 +469,7 @@ curateDatasetBtn.addEventListener('click', () => {
     metadatafiles.push(pathDescription)
   } else if (newDescriptionStatus.checked === true){
     descriptionStatus = true
-    pathDescription = path.join(__dirname, 'file_templates', 'dataset_description.csv')
+    pathDescription = path.join(__dirname, 'file_templates', 'dataset_description.xlsx')
     metadatafiles.push(pathDescription)
   } else {
     descriptionStatus = false
@@ -481,7 +481,7 @@ curateDatasetBtn.addEventListener('click', () => {
     metadatafiles.push(pathSubjects)
   } else if (newSubjectsStatus.checked === true){
     subjectsStatus = true
-    pathSubjects = path.join(__dirname, 'file_templates', 'subjects.csv')
+    pathSubjects = path.join(__dirname, 'file_templates', 'subjects.xlsx')
     metadatafiles.push(pathSubjects)
   } else {
     subjectsStatus = false
@@ -493,7 +493,7 @@ curateDatasetBtn.addEventListener('click', () => {
     metadatafiles.push(pathSamples)
   } else if (newSamplesStatus.checked === true){
     samplesStatus = true
-    pathSamples = path.join(__dirname, 'file_templates', 'samples.csv')
+    pathSamples = path.join(__dirname, 'file_templates', 'samples.xlsx')
     metadatafiles.push(pathSamples)
   } else {
    samplesStatus = false
@@ -1142,7 +1142,6 @@ function jsonToTableOrganized(table, jsonvar){
     var SPARCfolderid = SPARCfolder + '_org'
     var rowcount = document.getElementById(SPARCfolderid).rowIndex
     var pathlist = jsonvar[SPARCfolder]
-    console.log(pathlist)
     if (pathlist.length !== 0){
       console.log(SPARCfolder)
       var myheader = tableOrganized.rows[rowcount].cells[0]
@@ -1154,7 +1153,11 @@ function jsonToTableOrganized(table, jsonvar){
       tableOrganizedCount = tableOrganizedCount + 1
       var rownum = rowcount + i + 1
       var table_len = tableOrganizedCount
+      if (fs.lstatSync(pathlist[i]).isDirectory()) {
+        var row = table.insertRow(rownum).outerHTML="<tr id='row-org"+table_len+"'style='color: #000000;'><td id='name_row_org"+table_len+"'>"+ pathlist[i] +"</td> <td id='description_row_org"+table_len+"'>"+ "" +"</td> <td> </td></tr>";
+      } else {
       var row = table.insertRow(rownum).outerHTML="<tr id='row-org"+table_len+"'style='color: #000000;'><td id='name_row_org"+table_len+"'>"+ pathlist[i] +"</td> <td id='description_row_org"+table_len+"'>"+ "" +"</td> <td><input type='button' id='edit_button_org"+table_len+"' value='Edit description' class='edit' onclick='edit_row_org("+table_len+")'> <input type='button' id='save_button_org"+table_len+"' value='Save description' class='save' onclick='save_row_org("+table_len+")'> </td></tr>";
+      }
     }
   }
   return table
@@ -1223,7 +1226,11 @@ function insertFileToTable(table, path){
       tableNotOrganizedCount = tableNotOrganizedCount + 1
       var table_len=tableNotOrganizedCount
       var rownum = rowcount + i + 1
-      var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ path[i]+"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+      if (fs.lstatSync(path[i]).isDirectory()) {
+        var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ path[i]+"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+      } else {
+        var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ path[i]+"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+      }
     }
     return table
   }
@@ -1280,7 +1287,11 @@ function jsonToTableWithDescription(table, jsonvar){
 	      var rownum = rowcount + i + 1
 	      tableNotOrganizedCount = tableNotOrganizedCount + 1
 	      var table_len = tableNotOrganizedCount
-	      var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"' style='color: #000000;'><td id='name_row"+table_len+"'>"+ pathlist[i] + "</td><td id='description_row"+table_len+"'>"+ descriptionList[i] +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        if (fs.lstatSync(pathlist[i]).isDirectory()) {
+  	      var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"' style='color: #000000;'><td id='name_row"+table_len+"'>"+ pathlist[i] + "</td><td id='description_row"+table_len+"'>"+ descriptionList[i] +"</td><td> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        } else {
+          var row = table.insertRow(rownum).outerHTML="<tr id='row"+table_len+"' style='color: #000000;'><td id='name_row"+table_len+"'>"+ pathlist[i] + "</td><td id='description_row"+table_len+"'>"+ descriptionList[i] +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        }
        }
     }
   }
@@ -1356,7 +1367,11 @@ function dropAddToTable(e, myID){
           var rownum = rowcount + i + 1
   	    tableNotOrganizedCount = tableNotOrganizedCount + 1
   	    var table_len = tableNotOrganizedCount
-  	    var row = tableNotOrganized.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ f.path +"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        if (fs.lstatSync(f.path).isDirectory()) {
+    	    var row = tableNotOrganized.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ f.path +"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        } else {
+          var row = tableNotOrganized.insertRow(rownum).outerHTML="<tr id='row"+table_len+"'style='color: #000000;'><td id='name_row"+table_len+"'>"+ f.path +"</td><td id='description_row"+table_len+"'>"+ "" +"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit description' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save description' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete row' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+        }
        	i = i + 1
       }
   }
