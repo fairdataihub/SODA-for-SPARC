f45d2d53794346cf429b6c63fd47706ec72c8f82
bvhpatel <8172375+bvhpatel@users.noreply.github.com>
2020-02-05 11:06:07 -0800

    monkey patched all

--- src/pysoda/api.py
+++ src/pysoda/api.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from gevent import monkey; monkey.patch_all()
 from pysoda import submit_dataset_progress, curate_dataset_progress, save_file_organization, \
     import_file_organization, curate_dataset, preview_file_organization, delete_preview_file_organization, \
     bf_add_account, bf_account_list, bf_dataset_account, bf_account_details, \
@@ -9,6 +10,7 @@ from pysoda import submit_dataset_progress, curate_dataset_progress, save_file_o
 
 import sys
 import zerorpc
+from gevent import monkey; monkey.patch_all()
 
 class SodaApi(object):
 
