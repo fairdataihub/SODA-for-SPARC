d0f3c1afa3334cb89b9bba7cb11c426176c9529e
Tram Ngo <tramngo1603@gmail.com>
2020-03-04 16:02:31 -0800

    First draft of UI implementation

--- src/renderer-process/prepare-metadata/save-information.js
+++ src/renderer-process/prepare-metadata/save-information.js
@@ -19,3 +19,45 @@ document.querySelector('#addGrant').addEventListener('click', () => {
 document.querySelector('#selectGrant').addEventListener('click', () => {
     showSelectGrant()
 })
+
+function showDSInfo(){
+  document.getElementById('div-ds-dataset-info').style.display ='block';
+  document.getElementById('div-ds-contributor-info').style.display ='none';
+  document.getElementById('div-ds-misc-info').style.display ='none';
+  document.getElementById('div-ds-optional-info').style.display ='none';
+  document.getElementById('para-save-award-info').innerHTML = ""
+}
+function showContributorInfo(){
+  document.getElementById('div-ds-dataset-info').style.display ='none';
+  document.getElementById('div-ds-contributor-info').style.display ='block';
+  document.getElementById('div-ds-misc-info').style.display ='none';
+  document.getElementById('div-ds-optional-info').style.display ='none';
+  document.getElementById('para-save-award-info').innerHTML = ""
+}
+function showMiscInfo(){
+  document.getElementById('div-ds-dataset-info').style.display ='none';
+  document.getElementById('div-ds-contributor-info').style.display ='none';
+  document.getElementById('div-ds-misc-info').style.display ='block';
+  document.getElementById('div-ds-optional-info').style.display ='none';
+  document.getElementById('para-save-award-info').innerHTML = ""
+}
+function showOptionalInfo(){
+  document.getElementById('div-ds-dataset-info').style.display ='none';
+  document.getElementById('div-ds-contributor-info').style.display ='none';
+  document.getElementById('div-ds-misc-info').style.display ='none';
+  document.getElementById('div-ds-optional-info').style.display ='block';
+  document.getElementById('para-save-award-info').innerHTML = ""
+}
+
+document.querySelector('#ds-dataset-info').addEventListener('click', () => {
+    showDSInfo()
+})
+document.querySelector('#ds-contributor-info').addEventListener('click', () => {
+    showContributorInfo()
+})
+document.querySelector('#ds-misc-info').addEventListener('click', () => {
+    showMiscInfo()
+})
+document.querySelector('#ds-optional-info').addEventListener('click', () => {
+    showOptionalInfo()
+})
