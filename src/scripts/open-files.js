b76afa2dddf7b9c48510d6f7b6bf11805409af15
Tram Ngo <tramngo1603@gmail.com>
2020-09-09 17:57:24 -0700

    Added first UI look

---
+++ src/scripts/open-files.js
@@ -0,0 +1,23 @@
+// const importProgressFile = document.getElementById("import-progress")
+// const saveProgress = document.getElementById("save-progress")
+// const resetProgress = document.getElementById("reset-progress")
+
+// const BrowserWindow = require('electron')
+//// save progress as a json file
+// function savecurrentProgress() {
+//
+// }
+//
+// document.getElementById("save-progress").addEventListener("click", function() {
+//   const options = {
+//     title: 'Save File Organization',
+//     filters: [
+//       { name: 'JSON', extensions: ['json'] }
+//     ]
+//   }
+//   dialog.showSaveDialog(null, options, (filename) => {
+//     // console.log(filename)
+//     // console.log(location)
+//     event.sender.send('selected-saveorganizationfile', filename, location)
+//   })
+// })
