e6016178e4c960c9ae425c578be2642d4c53cadf
dev <dev@devs-MacBook-Pro.local>
2020-12-10 15:32:46 -0800

    Added app close for macos systems

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -105,8 +105,15 @@ ipcRenderer.on("update_available", () => {
 
 ipcRenderer.on("update_downloaded", () => {
   ipcRenderer.removeAllListeners("update_downloaded");
-  message.innerText =
-    "Update downloaded. It will be installed on the restart of the app. Restart now?";
+  if (process.platform == 'darwin')
+  {
+    message.innerText = "Update downloaded. It will be installed when you close and relaunch the app. Close the app now?";
+    document.getElementById("restart-button").innerText = "Close SODA";
+  }
+  else
+  {
+    message.innerText = "Update downloaded. It will be installed on the restart of the app. Restart the app now?";
+  }
   restartButton.classList.remove("hidden");
   notification.classList.remove("hidden");
 });
