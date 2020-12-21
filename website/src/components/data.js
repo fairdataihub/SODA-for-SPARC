9fc76bb7eb0320a07c15a745657bfcfa0d00831d
bvhpatel <8172375+bvhpatel@users.noreply.github.com>
2020-12-21 14:08:21 -0800

    website edits and suggestions

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -35,8 +35,9 @@ export const aboutObj = {
     id: 'about',
     imgStart: 'true',
     heading: 'About Us',
-    para1: 'We believe that all scientific data should be shared according to applicable standards based on the FAIR Data Principles. We also believe that doing so should be easy for researchers.',
-    para2: 'This is why we are developing SODA (Software to Organize Data Automatically), a desktop software that assists researchers in curating and sharing their data step by step such that it is intuitive, easy, efficient, and fun to do so.',
-    para3: 'Currently, we are developing SODA for the NIH SPARC Initiative, an NIH program that seeks to accelerate development of therapeutic devices that modulate electrical activity in nerves to improve organ function.',
+    para1: 'We are a team of researchers from the California Medical Innovations Institute who believe that all scientific research data should be curated and shared according to applicable standards based on the FAIR Data Principles. We also believe that doing so should be easy for the researchers collecting that data.',
+    para2: 'This is why we are developing SODA (Software to Organize Data Automatically), an open-source desktop software that assists researchers in curating and sharing their data step by step such that it is intuitive, easy, efficient, and fun to do so.',
+    para3: 'Our current effort is oriented towards developing SODA for SPARC, a version of SODA tailored to the needs of the NIH SPARC Initiative.',
+    para4: 'We are looking forward to build more versions of SODA to support curation and sharing for other research programs and data types beyond SPARC.',
     img: require('../images/banner.svg').default
 };
