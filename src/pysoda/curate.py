913d4980ee434b8115cc9f845579eb2e33be1866
Sanjay Soundarajan <sanjays344@mail.fresnostate.edu>
2021-04-06 11:07:50 -0700

    Bug fix for files with no extension

--- src/pysoda/curate.py
+++ src/pysoda/curate.py
@@ -1852,7 +1852,12 @@ def bf_get_existing_files_details(bf_folder):
     for file in bf_existing_files:
         file_id = file.id
         file_details = bf._api._get('/packages/' + str(file_id))
-        file_name_with_extension = verify_file_name(file_details["content"]["name"], file_details["extension"])
+        # file_name_with_extension = verify_file_name(file_details["content"]["name"], file_details["extension"])
+        if ("extension" not in file_details):
+            file_name = verify_file_name(file_details["content"]["name"], "")
+        else:
+            file_name = verify_file_name(file_details["content"]["name"], file_details["extension"])
+
         #file_extension = splitext(file_name_with_extension)[1]
         #file_name_with_extension = splitext(file.name)[0] + file_extension
         bf_existing_files_name_with_extension.append(file_name_with_extension)
