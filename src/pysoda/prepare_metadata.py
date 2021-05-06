77a9880de9535cfb5180df79d6e36789e621cb6c
Tram Ngo <tramngo1603@gmail.com>
2021-05-05 21:10:05 -0700

    Deleted non-mandatory columns

--- src/pysoda/prepare_metadata.py
+++ src/pysoda/prepare_metadata.py
@@ -304,31 +304,27 @@ def save_subjects_file(filepath, datastructure):
                 ws1[cell] = datastructure[i][j]
             else:
                 ws1[cell] = ""
-
             ws1[cell].font = Font(bold=False, size=11, name='Arial')
 
     # 4. delete empty columns
-    # deletedCols = []
+    deletedCols = []
     # # For row in range(datastructure): if row[cell].value !== None, break. If all None, delete entire column
-    # for col_cells in ws1.iter_cols(min_col=4, max_col=len(datastructure[0]), min_row=2, max_row=len(datastructure)):
-    #     flag = False
-    #     for cell in col_cells:
-    #         if cell is None:
-    #             flag = True
-    #             continue
-    #         else:
-    #             break
-    #         deletedCols.append(cell.col_idx)
+    for col_cells in ws1.iter_cols(min_col=4, max_col=len(datastructure[0]), min_row=2, max_row=len(datastructure)):
+        for cell in col_cells:
+            if cell.value != "":
+                break
+            deletedCols.append(cell.col_idx)
 
-    # deletedColumnNumber = 0
-    # for ind, col in enumerate(deletedCols):
-    #     if ind == 0:
-    #         ws1.delete_cols(col)
-    #     else:
-    #         col = col - deletedColumnNumber
-    #         ws1.delete_cols(col)
-    #     deletedColumnNumber += 1
-    #
+    deletedColumnNumber = 0
+    deletedCols = list(set(deletedCols))
+    for ind, col in enumerate(deletedCols):
+        if ind == 0:
+            ws1.delete_cols(col)
+        else:
+            col = col - deletedColumnNumber
+            ws1.delete_cols(col)
+        deletedColumnNumber += 1
+
     wb.save(destination)
 
 def save_samples_file(filepath, datastructure):
