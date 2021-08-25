f791c637d1c322edd4b44eef9b9edefb01dc50b9
Tram Ngo <tramngo1603@gmail.com>
2021-08-25 15:30:57 -0700

    Added strains and species UI change for samples

--- src/scripts/subjects-samples.js
+++ src/scripts/subjects-samples.js
@@ -405,6 +405,13 @@ async function addSpecies(ev, type) {
       $("#bootbox-"+type+"-species").css("background", "#f5f5f5");
       $(ev).html("<i class='pen icon'></i>Edit")
     }
+  } else {
+    $("#bootbox-"+type+"-species").css("display", "none");
+    if (type.includes("subject")) {
+      $("#button-add-species-subject").html(`<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle" width="14" height="14" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>Add strain`)
+    } else {
+      $("#button-add-species-sample").html(`<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle" width="14" height="14" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>Add strain`)
+    }
   }
 }
 
