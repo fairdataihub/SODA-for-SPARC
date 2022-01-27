17bd0c0a08f52129f15e3f47d9604cf1f6879365
Dorian Portillo <slugb0t@Dorians-MacBook-Pro.local>
2022-01-27 14:57:03 -0800

    attempting to implement a modal for progress bar

--- src/scripts/organize-dataset/curate-functions.js
+++ src/scripts/organize-dataset/curate-functions.js
@@ -1386,9 +1386,9 @@ async function openDropdownPrompt(ev, dropdown, show_timer = true) {
       $("body").removeClass("waiting");
       $(".svg-change-current-account.dataset").css("display", "block");
       $(".ui.active.green.inline.loader.small").css("display", "none");
-      ipcRenderer(
+      ipcRenderer.send(
         "track-event",
-        "Succes",
+        "Success",
         "User has succesfully chosen a dataset",
         defaultBfDataset
       );
