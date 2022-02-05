5a2bddef252326c778bc86d929ef2c81a11d451e
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-02-04 16:11:06 -0800

    Switch UI for selecting a local or pennsieve dataset for validation.

--- src/pyFlask/api.py
+++ src/pyFlask/api.py
@@ -14,8 +14,8 @@ userpath = expanduser("~")
 #PennsieveRemote = backend_pennsieve("N:organization:618e8dd9-f8d2-4dc4-9abb-c6aaab2e78a0")
 #root = PennsieveRemote("N:organization:618e8dd9-f8d2-4dc4-9abb-c6aaab2e78a0")
 #datasets = list(root.children)
-ds_location = Path(userpath +  "\\Desktop\\Pennsieve-dataset-206-version-1\\files")
-print(ds_location)
+# ds_location = Path(userpath +  "\\Desktop\\Pennsieve-dataset-206-version-1\\files")
+# print(ds_location)
 
 #blob = validate(path)
 
@@ -40,10 +40,11 @@ def api_ps_retrieve_dataset():
 @app.route("/api_validate_dataset_pipeline")
 @app.errorhandler(werkzeug.exceptions.BadRequest)
 def api_validate_dataset_pipeline():
-    print("In the route")
-    # get the account and dataset information
+    # get the dataset relative path
     ds_path = request.args.get("dataset-path")
+    # convert the path to absolute from user's home directory
     joined_path = path.join(userpath, ds_path.strip())
+    # convert to Path object for Validator to function properly
     norm_ds_path = Path(joined_path)
 
     # validate the dataset
@@ -51,7 +52,7 @@ def api_validate_dataset_pipeline():
     try:
         validation_result = val_dataset_local_pipeline(norm_ds_path)
     except:
-        return "Directory does not exist!", 400
+        return "Critical validation error!", 400
     
     print(validation_result)
     
