ef3b1a59949cdf1110f4b800dc58b7bdd8998eac
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-02-17 14:01:30 -0800

    Sweetalert backdrops showing. No exception on cancle skip modal.

--- src/scripts/organize-dataset/organizeDS.js
+++ src/scripts/organize-dataset/organizeDS.js
@@ -772,6 +772,73 @@ function selectAll(source) {
   }
 }
 
+function showParentSwal(duplicateArray) {
+  var tempFile = [];
+  var temp = duplicateArray.substring(1, duplicateArray.length - 1);
+  temp = temp.split(",");
+  let titleSwal = "";
+  let htmlSwal = "";
+  let html_word = "";
+
+  for (let i = 0; i < temp.length; i++) {
+    let lastSlash = temp[i].lastIndexOf("\\") + 1;
+    let fieldContainer = document.createElement("div");
+    if (lastSlash === 0) {
+      //in case it's on mac
+      lastSlash = temp[i].lastIndexOf("/") + 1;
+    }
+    //removes [ ] at end of string when passed through as JSON.stringify
+    tempFile[i] = temp[i].substring(lastSlash, temp[i].length);
+  }
+  if (tempFile[0].indexOf(".") != -1) {
+    titleSwal = "Duplicate file(s) detected";
+    htmlSwal =
+      "Files with the following names are already in the the current folder: ";
+    html_word = "Files";
+  } else {
+    titleSwal = "Duplicate folder(s) detected";
+    htmlSwal =
+      "Folders with the following names are already in the current folder: ";
+    html_word = "Folders";
+  }
+  var listElements = showItemsAsListBootbox(tempFile);
+  newList = JSON.stringify(temp).replace(/"/g, "");
+
+  return Swal.fire({
+    title: titleSwal,
+    icon: "warning",
+    showConfirmButton: false,
+    allowOutsideClick: false,
+    showCloseButton: true,
+    customClass: "wide-swal-auto",
+    backdrop: "rgba(0, 0, 0, 0.4)",
+    showClass: {
+      popup: "animate__animated animate__zoomIn animate__faster",
+    },
+    hideClass: {
+      popup: "animate_animated animate_zoomout animate__faster",
+    },
+    heightAuto: false,
+    html:
+      `
+    <div class="caption">
+      <p>${htmlSwal}<p><ul style="text-align: start;">${listElements}</ul></p></p>
+    </div>  
+    <div class="swal-button-container">
+      <button id="skip" class="btn skip-btn" onclick="handleDuplicateImports('skip', '` +
+      newList +
+      `')">Skip ${html_word}</button>
+      <button id="replace" class="btn replace-btn" onclick="handleDuplicateImports('replace', '` +
+      newList +
+      `')">Replace Existing ${html_word}</button>
+      <button id="rename" class="btn rename-btn" onclick="handleDuplicateImports('rename', '` +
+      newList +
+      `')">Import Duplicates</button>
+      <button id="cancel" class="btn cancel-btn" onclick="handleDuplicateImports('cancel')">Cancel</button>
+      </div>`,
+  });
+}
+
 function handleDuplicateImports(btnId, duplicateArray) {
   Swal.close();
   //creates the html for sweetalert
@@ -896,6 +963,68 @@ function handleDuplicateImports(btnId, duplicateArray) {
     Swal.fire({
       title: header,
       html: selectAll,
+      focusConfirm: false,
+      showCancelButton: true,
+      title: header,
+      backdrop: "rgba(0, 0, 0, 0.4)",
+      showClass: {
+        popup: "animate__animated animate__zoomIn animate__faster",
+      },
+      hideClass: {
+        popup: "animate_animated animate_zoomout animate__faster",
+      },
+      heightAuto: false,
+      didOpen: () => {
+        var confirm_button = document.getElementsByClassName("swal2-confirm");
+        confirm_button[0].disabled = true;
+        var select_all =
+          document.getElementById("container").parentElement.children[0]
+            .children[0];
+        var container = document.getElementById("container");
+        var check_boxes = container.querySelectorAll("input[type=checkbox]");
+        let checkedCount = 0;
+        check_boxes.forEach(function (element) {
+          element.addEventListener("change", function () {
+            if (this.checked) {
+              checkedCount += 1;
+              if (checkedCount === check_boxes.length) {
+                select_all.checked = true;
+              } else {
+                select_all.checked = false;
+              }
+              confirm_button[0].disabled = false;
+            } else {
+              checkedCount -= 1;
+              if (checkedCount === check_boxes.length) {
+                select_all.checked = true;
+              } else {
+                select_all.checked = false;
+              }
+              let one_checked = false;
+              for (let i = 0; i < check_boxes.length; i++) {
+                if (check_boxes[i].checked) {
+                  one_checked = true;
+                  break;
+                }
+              }
+              if (one_checked === true) {
+                confirm_button[0].disabled = false;
+              } else {
+                confirm_button[0].disabled = true;
+                select_all.checked = false;
+              }
+            }
+          });
+        });
+        select_all.addEventListener("change", function () {
+          var check_boxes = container.querySelectorAll("input[type=checkbox]");
+          if (this.checked) {
+            confirm_button[0].disabled = false;
+          } else {
+            confirm_button[0].disabled = true;
+          }
+        });
+      },
     }).then((result) => {
       if (result.isConfirmed) {
         let container = document.getElementById("container");
@@ -931,6 +1060,10 @@ function handleDuplicateImports(btnId, duplicateArray) {
           }
         }
       }
+      if (!result.isConfirmed) {
+        showParentSwal(duplicateArray);
+        return
+      }
       //unless all files are skipped it will prompt again on what to do
       //with duplicate files
       var listElements = showItemsAsListBootbox(fileName);
@@ -964,6 +1097,7 @@ function handleDuplicateImports(btnId, duplicateArray) {
           hideClass: {
             popup: "animate_animated animate_zoomout animate__faster",
           },
+          heightAuto: false,
           html:
             `
           <div class="caption">
@@ -1033,6 +1167,85 @@ function handleDuplicateImports(btnId, duplicateArray) {
           popup: "animate_animated animate_zoomout animate__faster",
         },
         html: container,
+        didOpen: () => {
+          var confirm_button = document.getElementsByClassName("swal2-confirm");
+          var container = document.getElementById("container");
+          let input_fields = container.querySelectorAll("input[type=text]");
+          let fileExt = input_fields[0].id.lastIndexOf(".");
+          var keyCheck;
+          if (fileExt === -1) {
+            //working with a folder
+            keyCheck = myPath["folders"];
+          } else {
+            //working with a file
+            keyCheck = myPath["files"];
+          }
+
+          confirm_button[0].disabled = true;
+          input_fields.forEach(function (element) {
+            element.addEventListener("input", function () {
+              if (fileExt != -1) {
+                let first_ext = element.id.lastIndexOf(".");
+                let extType = element.id.substring(
+                  first_ext,
+                  element.id.length
+                );
+                if (
+                  element.value === "" ||
+                  keyCheck.hasOwnProperty(element.value + extType)
+                ) {
+                  confirm_button[0].disabled = true;
+                } else {
+                  let one_input = false;
+                  for (let i = 0; i < input_fields.length; i++) {
+                    let file_Ext = input_fields[i].id.lastIndexOf(".");
+                    extType = input_fields[i].id.substring(
+                      file_Ext,
+                      input_fields[i].id.length
+                    );
+                    if (
+                      input_fields[i].value === "" ||
+                      keyCheck.hasOwnProperty(input_fields[i].value + extType)
+                    ) {
+                      one_input = true;
+                      break;
+                    }
+                  }
+                  if (one_input === true) {
+                    confirm_button[0].disabled = true;
+                  } else {
+                    input_fields.forEach(function (element) { });
+                    confirm_button[0].disabled = false;
+                  }
+                }
+              } else {
+                //working with folders
+                if (
+                  element.value === "" ||
+                  keyCheck.hasOwnProperty(element.value)
+                ) {
+                  confirm_button[0].disabled = true;
+                } else {
+                  let one_input = false;
+                  for (let i = 0; i < input_fields.length; i++) {
+                    if (
+                      input_fields[i].value === "" ||
+                      keyCheck.hasOwnProperty(element.value)
+                    ) {
+                      one_input = true;
+                      break;
+                    }
+                  }
+                  if (one_input === true) {
+                    confirm_button[0].disabed = true;
+                  } else {
+                    confirm_button[0].disabled = false;
+                  }
+                }
+              }
+            });
+          });
+        },
         preConfirm: () => {
           //check the same name isn't being used
           var fileNames = [];
@@ -1239,6 +1452,14 @@ function handleDuplicateImports(btnId, duplicateArray) {
       allowOutsideClick: false,
       cancelButtonText: "Cancel",
       showCancelButton: true,
+      backdrop: "rgba(0, 0, 0, 0.4)",
+      showClass: {
+        popup: "animate__animated animate__zoomIn animate__faster",
+      },
+      hideClass: {
+        popup: "animate_animated animate_zoomout animate__faster",
+      },
+      heightAuto: false,
       didOpen: () => {
         var confirm_button = document.getElementsByClassName("swal2-confirm");
         confirm_button[0].disabled = true;
@@ -1551,6 +1772,7 @@ function addFilesfunction(
       hideClass: {
         popup: "animate_animated animate_zoomout animate__faster",
       },
+      heightAuto: false,
       html:
         `
       <div class="caption">
