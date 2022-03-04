faeb6c2bd9725512ea3d644184d889a03af38b9d
Tram Ngo <tramngo1603@gmail.com>
2022-03-03 16:15:09 -0800

    Added Add cols/Rename cols to manifest

--- src/scripts/metadata-files/manifest.js
+++ src/scripts/metadata-files/manifest.js
@@ -171,7 +171,6 @@ $(document).ready(function () {
           "*": "{{columnHeader}}",
         },
       })["Sheet1"];
-
       Swal.fire({
         title: "Edit the manifest file below:",
         html: "<div id='div-manifest-edit'></div>",
@@ -188,8 +187,12 @@ $(document).ready(function () {
           Swal.hideLoading();
         },
       }).then((result) => {
-        // write this new json to existing manifest.json file
-        jsonManifest = JSON.stringify(table1.getJson());
+        // sort the updated json object (since users might have added new columns)
+        let manifestHeaders = table1.getHeaders().split(",");
+        let manifestEntries = table1.getData();
+        let sortedJSON = processManifestInfo(manifestHeaders, manifestEntries);
+        // // write this new json to existing manifest.json file
+        jsonManifest = JSON.stringify(sortedJSON);
         // convert manifest.json to existing manifest.xlsx file
         convertJSONToXlsx(JSON.parse(jsonManifest), selectedManifestFilePath);
       });
@@ -221,52 +224,84 @@ function convertJSONToXlsx(jsondata, excelfile) {
 
 var table1;
 function loadManifestFileEdits(jsondata) {
-  // let rawdata = fs.readFileSync(jsonPath);
-  // let jsondata = JSON.parse(rawdata);
+  let columns = Object.keys(jsondata[0]);
+  let columnList = [];
+  for (let i=0; i<columns.length;i++) {
+    let subColumn = {
+      type:"text",
+      width: "120px",
+      name: columns[i],
+      title: columns[i],
+      readOnly: false,
+    }
+    columnList.push(subColumn)
+  }
   // After ID in pop has been initiated, initialize jspreadsheet
   table1 = jspreadsheet(document.getElementById("div-manifest-edit"), {
     data: jsondata.slice(1),
-    columns: [
-      {
-        type: "text",
-        width: "150px",
-        name: "filename",
-        title: "filename",
-        readOnly: true,
-      },
-      {
-        type: "text",
-        width: "100px",
-        name: "timestamp",
-        title: "timestamp",
-        readOnly: true,
-      },
-      {
-        type: "text",
-        width: "150px",
-        name: "description",
-        title: "description",
-        readOnly: false,
-      },
-      {
-        type: "text",
-        width: "100px",
-        name: "file type",
-        title: "file type",
-        readOnly: true,
-      },
-      {
-        type: "text",
-        width: "150px",
-        name: "Additional Metadata",
-        title: "Additional Metadata",
-        readOnly: false,
-      },
-    ],
+    columns: columnList,
     contextMenu: function (obj, x, y, e) {
-      return [];
-    },
+      var items = [];
+      if (y == null) {
+        // Insert a new column
+        if (obj.options.allowInsertColumn == true) {
+          items.push({
+              title:obj.options.text.insertANewColumnBefore,
+              onclick:function() {
+                  obj.insertColumn(1, parseInt(x), 1);
+                  $("#div-manifest-edit").find("table").find("thead").find("td").dblclick(function(e) {
+                    e.target.contentEditable = true;
+                    e.target.innerText = "";
+                    e.target.focus()
+                  });
+              }
+          });
+        }
+        if (obj.options.allowInsertColumn == true) {
+         items.push({
+             title:obj.options.text.insertANewColumnAfter,
+             onclick:function() {
+                 obj.insertColumn(1, parseInt(x), 0);
+                 $("#div-manifest-edit").find("table").find("thead").find("td").dblclick(function(e) {
+                   e.target.contentEditable = true;
+                   e.target.innerText = "";
+                   e.target.focus()
+                 });
+             }
+         });
+       }
+       // Delete a column
+       if (obj.options.allowDeleteColumn == true) {
+         items.push({
+             title:obj.options.text.deleteSelectedColumns,
+             onclick:function() {
+                 obj.deleteColumn(obj.getSelectedColumns().length ? undefined : parseInt(x));
+             }
+         });
+       }
+    }
+    return items;
+    }
+  })
+  $("#div-manifest-edit").find("table").find("thead").find("td").dblclick(function(e) {
+    e.target.contentEditable = true;
+    e.target.innerText = "";
+    e.target.focus()
   });
+}
+
+const processManifestInfo = (headers, data) => {
+  let sortedArr = [];
+  // sort json data by appending ordered entries (by columns) to each object's element
+  for (let i=0; i<data.length;i++) {
+    let temp = {};
+    for (let j=0; j<headers.length;j++) {
+      let header = headers[j];
+      temp[header] = data[i][j];
+    }
+    sortedArr.push(temp)
+  }
+  return sortedArr
 }
 
 var localDatasetFolderPath = "";
