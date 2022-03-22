9f2390b1d203e5fe25b2a6c9c818332c8599c722
Aaron Marroquin <aaron.marroquin96@gmail.com>
2022-03-22 14:11:45 -0700

    Gets amount of files in lcoal directory

--- src/scripts/others/renderer.js
+++ src/scripts/others/renderer.js
@@ -6982,6 +6982,25 @@ function initiate_generate() {
   }
 }
 
+// counts the amount of files in a local dataset's generation location 
+// FOR NOW ASSUME WE ARE ALWAYS HANDLING NEW GENERATION 
+const getLocallyGeneratedFileCount = async (generationLocation) => {
+  // TODO: make sure that the target location exists 
+
+  // get the amount of files in the directory 
+  let generatedFiles = await fs.readdir(generationLocation)
+
+  console.log("The amount of files generated is: ", generatedFiles)
+
+
+  return generatedFiles
+
+}
+
+const getLocallyGeneratedDatasetSize = async (generationLocation) => {
+
+}
+
 const show_curation_shortcut = () => {
   Swal.fire({
     backdrop: "rgba(0,0,0, 0.4)",
