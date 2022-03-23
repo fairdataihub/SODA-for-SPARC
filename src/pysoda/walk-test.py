b61e8d9af9751e25138f6930c7c9423d9cd2acc8
Aaron Marroquin <aaron.marroquin96@gmail.com>
2022-03-23 15:51:42 -0700

    Testing bucketization of the local dataset upload

---
+++ src/pysoda/walk-test.py
@@ -0,0 +1,45 @@
+import os
+
+
+BUCKET_SIZE = 500
+
+# top down scan through dataset to upload each file/folder
+for root, dirs, files in os.walk("C:\\Users\\CMarroquin\\status-barr-local", topdown=True):
+    print("Root is:", root)
+    print("Dirs are: ", dirs)
+    print("files are: ", files)
+    if len(files) > 500: 
+        # bucket the upload 
+        start_index = 0
+        end_index = 499
+        print("Too many files, bucketing the upload as such: ", files[])
+        # store the aggregate of the amount of files in the folder
+        total_files = len(files)
+
+        # while startIndex < files.length
+        while start_index < total_files:
+            # set the endIndex to startIndex plus 750
+            end_index = start_index + BUCKET_SIZE - 1
+
+            # check if the endIndex is out of bounds
+            if end_index >= total_files:
+                # if so set end index to files.length - 1
+                end_index = len(files) - 1
+
+                # get the 750 files between startIndex and endIndex (inclusive of endIndex)
+                upload_bucket = files[start_index : end_index + 1]
+
+                # clear the pennsieve queue for successive batches
+                # clear_queue()
+
+                # upload the files
+                # bf_folder.upload(*upload_bucket)
+                print("Uploading these files: ", root, upload_bucket)
+
+                # update the global that tracks the amount of files that have been successfully uploaded
+                # main_curation_uploaded_files += BUCKET_SIZE
+
+                # update the start_index to end_index + 1
+                start_index = end_index + 1
+    else:
+        print("Uploading all the files in the current directory: ", root, files)
