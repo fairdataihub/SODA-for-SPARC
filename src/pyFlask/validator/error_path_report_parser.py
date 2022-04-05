81d026e9a8ef1f02323a1f60d04f811cee21b139
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-04-05 13:24:04 -0700

    Update comments

--- src/pyFlask/validator/error_path_report_parser.py
+++ src/pyFlask/validator/error_path_report_parser.py
@@ -92,11 +92,9 @@ error_path_report = {
 }
 
 
-
-
 # return the errors from the error_path_report that should be shown to the user.
 # as per Tom (developer of the Validator) for any paths (the keys in the Path_Error_Report object)
-# with common prefixes, only return the one that doesn't have any errors in its subpaths. 
+# return the ones that do not have any errors in their subpaths. 
 # e.g., If given #/meta and #/meta/technique keys only return #/meta/technique (as this group doesn't have any subpaths)
 def get_target_errors(error_path_report):
 
@@ -142,58 +140,4 @@ def get_path_prefix(path):
     return path[0: final_slash_idx + 1]
     
 
-    
-
 print(get_target_errors(error_path_report))   
-
-
-{
-    '#/': {
-        'error_count': 1,
-        'messages': ["'path_metadata' is a required property"]
-    },
-    '#/contributors': {
-        'error_count': 1,
-        'messages': ["[{'contributor_name': 'Marroquin, Christopher', 'contributor_orcid': 'https://orcid.org/0000-0002-3399-1731', 'contributor_affiliation': 'https://ror.org/0168r3w48', 'contributor_role': ['PrincipalInvestigator'], 'first_name': 'Christopher', 'last_name': 'Marroquin', 'id': 'https://orcid.org/0000-0002-3399-1731'}] is not valid under any of the given schemas"]
-    },
-    '#/id': {
-        'error_count': 1,
-        'messages': ["'cgeNh_H8zjDDsGcJn2wlicHHZcjoavLjuiZxzsRAw2ix6Tp4Bwutpuj2ykK8i1JamSX9nurlL6MSLF7LlUicLA:C:/Users/CMarroquin/temp-datasets/DatasetTemplate' does not match '^N:dataset:'"]
-    },
-    '#/inputs/dataset_description_file/contributors': {
-        'error_count': 1,
-        'messages': ["[{'contributor_name': 'Marroquin, Christopher', 'contributor_orcid': 'https://orcid.org/0000-0002-3399-1731', 'contributor_affiliation': 'https://ror.org/0168r3w48', 'contributor_role': ['PrincipalInvestigator']}] is not valid under any of the given schemas"]
-    },
-    '#/inputs/manifest_file/-1/contents/manifest_records/-1': {
-        'error_count': 1,
-        'messages': ["{'description': 'j'} is not valid under any of the given schemas"]
-    },
-    '#/inputs/manifest_file/-1/uri_api': {
-        'error_count': 1,
-        'messages': ["'file://C:/Users/CMarroquin/temp-datasets/DatasetTemplate/manifest.xlsx' does not match '^(https?):\\\\/\\\\/([^\\\\s\\\\/]+)\\\\/([^\\\\s]*)'"]
-    },
-    '#/inputs/manifest_file/-1/uri_human': {
-        'error_count': 1,
-        'messages': ["'file://C:/Users/CMarroquin/temp-datasets/DatasetTemplate/manifest.xlsx' does not match '^(https?):\\\\/\\\\/([^\\\\s\\\\/]+)\\\\/([^\\\\s]*)'"]
-    },
-    '#/meta/techniques': {
-        'error_count': 1,
-        'messages': ['[] is too short']
-    },
-    '#/meta/timestamp_created': {
-        'error_count': 1,
-        'messages': ["None is not of type 'string'"]
-    },
-    '#/meta/uri_api': {
-        'error_count': 1,
-        'messages': ["'file://C:/Users/CMarroquin/temp-datasets/DatasetTemplate' does not match '^https://api\\\\.pennsieve\\\\.io/(datasets|packages)/'"]
-    },
-    '#/meta/uri_human': {
-        'error_count': 1,
-        'messages': ["'file://C:/Users/CMarroquin/temp-datasets/DatasetTemplate' does not match '^https://app\\\\.pennsieve\\\\.io/N:organization:'"]
-    },
-    '#/specimen_dirs': {
-        'error_count': 3,
-        'messages': ["'records' is a required property", 'No folder for sample sam-1', "There are specimens that have no corresponding directory!\n{'sub-1_sam-1', 'sub-1'}"]
-    }
-}
