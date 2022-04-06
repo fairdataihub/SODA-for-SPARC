f34fe5384f5a55cf3efcec007864e49f4e3f4d41
JacobDClark <jacobcs@zoho.com>
2022-04-05 18:25:49 -0700

    broke out sample metadata fields into compartments by subject when copying

--- src/scripts/guided-mode/guided-curate-dataset.js
+++ src/scripts/guided-mode/guided-curate-dataset.js
@@ -876,17 +876,18 @@ const openCopySampleMetadataPopup = async (clickedSampleCopyMetadataButton) => {
     .text();
 
   const initialCopyFromMetadata = `
-        <div class="field text-left">
-          <div class="ui radio checkbox">
-            <input type="radio" name="copy-from" value="${copyFromSampleMetadataID}" checked="checked">
-            <label>${copyFromSampleMetadataID}</label>
+          <div class="field text-left">
+            <div class="ui radio checkbox">
+              <input type="radio" name="copy-from" value="${copyFromSampleMetadataID}" checked="checked">
+              <label>${copyFromSampleMetadataID}</label>
+            </div>
           </div>
-        </div>
-      `;
+        `;
+  const subjectsArray = guidedGetSubjects();
 
   let samplesArray = [];
   //Add all subjects in sodaJSONObj to samplesArray
-  for (let subject of guidedGetSubjects()) {
+  for (let subject of subjectsArray) {
     let subjectsSamples = guidedGetSubjectSamples(subject);
     samplesArray = samplesArray.concat(subjectsSamples);
   }
@@ -895,44 +896,56 @@ const openCopySampleMetadataPopup = async (clickedSampleCopyMetadataButton) => {
     .filter((sample) => sample !== copyFromSampleMetadataID)
     .map((sample) => {
       return `
-          <div class="field text-left">
-            <div class="ui radio checkbox">
-              <input type="radio" name="copy-from" value="${sample}">
-              <label>${sample}</label>
-            </div>
-          </div>`;
+        <div class="field text-left">
+          <div class="ui radio checkbox">
+            <input type="radio" name="copy-from" value="${sample}">
+            <label>${sample}</label>
+          </div>
+        </div>`;
     })
     .join("\n");
 
-  const copyToMetadata = samplesArray
-    .map((sample) => {
-      return `
-          <div class="field text-left">
-            <div class="ui checkbox">
+  const copyToMetadata = subjectsArray
+    .map((subject) => {
+      //get the samples of the subject being mapped over
+      let subjectSamples = guidedGetSubjectSamples(subject);
+      //create a checkbox for each of the samples
+      let subjectSamplesCheckboxes = subjectSamples
+        .map((sample) => {
+          return `
+            <div class="field text-left">
+              <div class="ui checkbox">
               <input type="checkbox" name="copy-to" value="${sample}">
               <label>${sample}</label>
+              </div>
             </div>
-          </div>`;
+          `;
+        })
+        .join("\n");
+      return `
+        ${subject}
+        ${subjectSamplesCheckboxes}
+      `;
     })
     .join("\n");
 
   const copyMetadataElement = `
-      <div class="space-between">
-        <div class="ui form">
-          <div class="grouped fields">
-            <label class="guided--form-label text-left">Which sample would you like to copy metadata from?</label>
-            ${initialCopyFromMetadata}
-            ${copyFromMetadata}
+        <div class="space-between">
+          <div class="ui form">
+            <div class="grouped fields">
+              <label class="guided--form-label med text-left">Which sample would you like to copy metadata from?</label>
+              ${initialCopyFromMetadata}
+              ${copyFromMetadata}
+            </div>
+          </div>
+          <div class="ui form">
+            <div class="grouped fields">
+              <label class="guided--form-label med text-left">Which samples would you like to copy metadata to?</label>
+              ${copyToMetadata}
+            </div>
           </div>
         </div>
-        <div class="ui form">
-          <div class="grouped fields">
-            <label class="guided--form-label text-left">Which samples would you like to copy metadata to?</label>
-            ${copyToMetadata}
-          </div>
-        </div>
-      </div>
-      `;
+        `;
   swal
     .fire({
       width: 950,
@@ -1025,14 +1038,14 @@ const openCopySubjectMetadataPopup = async (
   <div class="space-between">
     <div class="ui form">
       <div class="grouped fields">
-        <label class="guided--form-label text-left">Which subject would you like to copy metadata from?</label>
+        <label class="guided--form-label med text-left">Which subject would you like to copy metadata from?</label>
         ${initialCopyFromMetadata}
         ${copyFromMetadata}
       </div>
     </div>
     <div class="ui form">
       <div class="grouped fields">
-        <label class="guided--form-label text-left">Which subjects would you like to copy metadata to?</label>
+        <label class="guided--form-label med text-left">Which subjects would you like to copy metadata to?</label>
         ${copyToMetadata}
       </div>
     </div>
