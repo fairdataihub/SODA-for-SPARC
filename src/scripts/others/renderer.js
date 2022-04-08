5c353b604b50e0d531587ca5fb6083d4dc9d1e2b
Aaron Marroquin <aaron.marroquin96@gmail.com>
2022-04-07 18:46:20 -0700

    Determine dataset llocation handles all routes

--- src/scripts/others/renderer.js
+++ src/scripts/others/renderer.js
@@ -1115,7 +1115,7 @@ ipcRenderer.on(
               didOpen: () => {
                 Swal.showLoading();
               },
-            }).then((result) => {});
+            }).then((result) => { });
             generateSubjectsFileHelper(false);
           }
         });
@@ -1131,7 +1131,7 @@ ipcRenderer.on(
           didOpen: () => {
             Swal.showLoading();
           },
-        }).then((result) => {});
+        }).then((result) => { });
         generateSubjectsFileHelper(false);
       }
     }
@@ -1185,7 +1185,7 @@ async function generateSubjectsFileHelper(uploadBFBoolean) {
     didOpen: () => {
       Swal.showLoading();
     },
-  }).then((result) => {});
+  }).then((result) => { });
 
   client.invoke(
     "api_save_subjects_file",
@@ -1277,7 +1277,7 @@ ipcRenderer.on(
               didOpen: () => {
                 Swal.showLoading();
               },
-            }).then((result) => {});
+            }).then((result) => { });
             generateSamplesFileHelper(uploadBFBoolean);
           }
         });
@@ -1293,7 +1293,7 @@ ipcRenderer.on(
           didOpen: () => {
             Swal.showLoading();
           },
-        }).then((result) => {});
+        }).then((result) => { });
         generateSamplesFileHelper(uploadBFBoolean);
       }
     }
@@ -1347,7 +1347,7 @@ async function generateSamplesFileHelper(uploadBFBoolean) {
     didOpen: () => {
       Swal.showLoading();
     },
-  }).then((result) => {});
+  }).then((result) => { });
 
   // new client that has a longer timeout
   let clientLongTimeout = new zerorpc.Client({
@@ -1865,7 +1865,7 @@ async function loadTaxonomySpecies(commonName, destinationInput) {
     didOpen: () => {
       Swal.showLoading();
     },
-  }).then((result) => {});
+  }).then((result) => { });
   await client.invoke(
     "api_load_taxonomy_species",
     [commonName],
@@ -2582,9 +2582,9 @@ function detectEmptyRequiredFields(funding) {
   var emptyArray = [dsSatisfied, conSatisfied, protocolSatisfied];
   var emptyMessageArray = [
     "- Missing required fields under Dataset Info section: " +
-      dsEmptyField.join(", "),
+    dsEmptyField.join(", "),
     "- Missing required fields under Contributor Info section: " +
-      conEmptyField.join(", "),
+    conEmptyField.join(", "),
     "- Missing required item under Article(s) and Protocol(s) Info section: At least one protocol url",
   ];
   var allFieldsSatisfied = true;
@@ -6397,9 +6397,9 @@ document
     for (var highLevelFol in sodaJSONObj["dataset-structure"]["folders"]) {
       if (
         "manifest.xlsx" in
-          sodaJSONObj["dataset-structure"]["folders"][highLevelFol]["files"] &&
+        sodaJSONObj["dataset-structure"]["folders"][highLevelFol]["files"] &&
         sodaJSONObj["dataset-structure"]["folders"][highLevelFol]["files"][
-          "manifest.xlsx"
+        "manifest.xlsx"
         ]["forTreeview"]
       ) {
         delete sodaJSONObj["dataset-structure"]["folders"][highLevelFol][
@@ -6912,7 +6912,7 @@ async function initiate_generate() {
             "track-event",
             "Success",
             PrepareDatasetsAnalyticsPrefix.CURATE +
-              " - Step 7 - Generate - Dataset - Number of Files",
+            " - Step 7 - Generate - Dataset - Number of Files",
             `${datasetUploadSession.id}`,
             uploadedFiles
           );
@@ -6922,7 +6922,7 @@ async function initiate_generate() {
             "track-event",
             "Success",
             PrepareDatasetsAnalyticsPrefix.CURATE +
-              " - Step 7 - Generate - Dataset - Size",
+            " - Step 7 - Generate - Dataset - Size",
             `${datasetUploadSession.id}`,
             increaseInFileSize
           );
@@ -6990,23 +6990,44 @@ const get_num_files_and_folders = (dataset_folders) => {
 };
 
 function determineDatasetDestination() {
-  console.log(sodaJSONObj);
   if (sodaJSONObj["generate-dataset"]) {
     if (sodaJSONObj["generate-dataset"]["destination"]) {
       let destination = sodaJSONObj["generate-dataset"]["destination"];
-
       if (destination === "bf" || destination === "Pennsieve") {
-        console.log("Going to return pennsieve");
-        return [
-          sodaJSONObj["bf-dataset-selected"]["dataset-name"],
-          "Pennsieve",
-        ];
+        // updating an existing dataset on Pennsieve
+        if (sodaJSONObj["bf-dataset-selected"]) {
+          return [
+            sodaJSONObj["bf-dataset-selected"]["dataset-name"],
+            "Pennsieve"
+          ]
+        } else {
+          return [
+            // get dataset name,
+            document.querySelector("#inputNewNameDataset").value,
+            "Pennsieve"
+          ]
+        }
       } else {
-        return "local";
+        // replacing files in an existing local dataset
+        if (sodaJSONObj["generate-dataset"]["dataset-name"]) {
+          return [
+            sodaJSONObj["generate-dataset"]["dataset-name"],
+            "local"
+          ]
+        } else {
+          // creating a new dataset from an existing local dataset
+          return [
+            document.querySelector("#inputNewNameDataset").value,
+            "local"
+          ]
+        }
       }
     }
   } else {
-    return "local";
+    return [
+      document.querySelector("#inputNewNameDataset").value,
+      "local"
+    ];
   }
 }
 
