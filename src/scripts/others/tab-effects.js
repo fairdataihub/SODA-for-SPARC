50b4dcf8137022917da42a52e5795fe0efe0e6ff
slugb0t <wheresdorian@gmail.com>
2022-04-22 14:07:31 -0700

    update on root folder check

--- src/scripts/others/tab-effects.js
+++ src/scripts/others/tab-effects.js
@@ -1786,16 +1786,14 @@ const verify_sparc_folder = (root_folder_path) => {
   ];
   valid_dataset = false;
   fs.readdirSync(root_folder_path).forEach((file) => {
-    if (highLevelFolders.includes(file)) {
+    if (highLevelFolders.includes(file) || possible_metadata_files.includes(path.parse(file).name)) {
+      console.log(file)
       valid_dataset = true;
     } else {
+      console.log("not included")
+      console.log(file)
       valid_dataset = false;
       return valid_dataset;
-    }
-    for (item in possible_metadata_files) {
-      if (item.indexOf(file) != -1) {
-        valid_dataset = true;
-      }
     }
   });
   return valid_dataset;
