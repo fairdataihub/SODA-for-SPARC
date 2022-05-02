7a02d955d6aa185f8d84c3bc1e4ce38d2b7d301e
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-05-02 09:51:53 -0700

    Env values for port and host

--- src/pyflask/app.py
+++ src/pyflask/app.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import json
 import logging
 import logging.handlers
+from os import getenv 
 
 from flask import Flask, request
 # from flask_cors import CORS
@@ -15,5 +16,5 @@ app = Flask(__name__)
 
 api.init_app(app)
 
-app.run(debug=True)
+app.run(debug=True, host=getenv('HOST'), port=getenv('PORT'))
 
