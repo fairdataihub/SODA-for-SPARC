1c021d987447ed4965e373d963ce4a4431b0aa46
unknown <jacobcs@zoho.com>
2022-05-13 10:28:48 -0700

    Fixed bug causing dataset subtitle max character count to be undervalued by 1

--- src/scripts/manage-dataset/manage-dataset.js
+++ src/scripts/manage-dataset/manage-dataset.js
@@ -716,7 +716,7 @@ $("#button-add-permission-team").click(() => {
 // Character count for subtitle //
 function countCharacters(textelement, pelement) {
   var textEntered = textelement.value;
-  var counter = 255 - textEntered.length;
+  var counter = 256 - textEntered.length;
   pelement.innerHTML = counter + " characters remaining";
   return textEntered.length;
 }
