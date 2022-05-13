8c8a0c96358efd6485e19bf78634c8de49cb5c8a
Aaron Marroquin <aaron.marroquin96@gmail.com>
2022-05-12 21:49:02 -0700

    refactor: set template path api method added

--- src/pysoda/api.py
+++ src/pysoda/api.py
@@ -74,6 +74,7 @@ from prepare_metadata import (
     import_bf_RC,
     upload_RC_file,
     delete_manifest_dummy_folders,
+    set_template_path
 )
 
 from organize_datasets import (
@@ -587,6 +588,13 @@ class SodaApi(object):
         except Exception as e:
             raise e
 
+    ### Sets the TEMPLATE_PATH using SODA-for-SPARC's basepath so that the prepare_metadata section can find 
+    ### the templates stored in file_templates direcotory
+    def api_set_template_path(self, soda_base_path):
+        try:
+            return set_template_path(soda_base_path)
+        except Exception as e:
+            raise e
 
 ### Connect to Electron-Python
 def parse_port():
