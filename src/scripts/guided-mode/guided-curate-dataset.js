22120048aa759d06dd5736d5b36975414afff41a
unknown <jacobcs@zoho.com>
2022-05-18 12:08:30 -0700

    added page loading functions for the protocol page

--- src/scripts/guided-mode/guided-curate-dataset.js
+++ src/scripts/guided-mode/guided-curate-dataset.js
@@ -1512,6 +1512,14 @@ const highLevelFolderPageData = {
     pathSuffix: "docs/",
     backPageId: "guided-docs-folder-tab",
   },
+  protocol: {
+    headerText:
+      "Virtually structure your protocol folder in the interface below.",
+    contentsText:
+      "Your protocol folder should contain lorem ipsum foo bar random instructional text will go here",
+    pathSuffix: "protocol/",
+    backPageId: "guided-protocol-folder-tab",
+  },
 };
 const generateHighLevelFolderSubFolderPageData = (
   sampleOrSubject,
@@ -3856,6 +3864,58 @@ $("#guided-button-no-docs-data").on("click", () => {
   }
 });
 
+/*********** Protocol page functions ***********/
+$("#guided-button-has-protocol-data").on("click", () => {
+  if (datasetStructureJSONObj["folders"]["protocol"] == undefined)
+    datasetStructureJSONObj["folders"]["protocol"] = {
+      folders: {},
+      files: {},
+      type: "",
+      action: [],
+    };
+  $("#guided-file-explorer-elements").appendTo(
+    $("#guided-user-has-protocol-data")
+  );
+  updateFolderStructureUI(highLevelFolderPageData.protocol);
+});
+$("#guided-button-no-protocol-data").on("click", () => {
+  //ask user to confirm they would like to delete protocol folder if it exists
+  if (datasetStructureJSONObj["folders"]["protocol"] != undefined) {
+    Swal.fire({
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      title:
+        "Reverting your decision will wipe out any changes you have made to the protocol folder.",
+      text: "Are you sure you would like to delete your protocol folder progress?",
+      icon: "warning",
+      showConfirmButton: true,
+      confirmButtonText: "Delete",
+      confirmButtonColor: "#3085d6 !important",
+      showCancelButton: true,
+      focusCancel: true,
+      reverseButtons: reverseSwalButtons,
+      heightAuto: false,
+      customClass: "swal-wide",
+      backdrop: "rgba(0,0,0, 0.4)",
+      showClass: {
+        popup: "animate__animated animate__zoomIn animate__faster",
+      },
+      hideClass: {
+        popup: "animate__animated animate__zoomOut animate__faster",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //User agrees to delete protocol folder
+        delete datasetStructureJSONObj["folders"]["protocol"];
+      } else {
+        //User cancels
+        //reset button UI to how it was before the user clicked no protocol files
+        $("#guided-button-has-protocol-data").click();
+      }
+    });
+  }
+});
+
 const getTagsFromTagifyElement = (tagifyElement) => {
   return Array.from(tagifyElement.getTagElms()).map((tag) => {
     return tag.textContent;
@@ -4315,6 +4375,10 @@ $(document).ready(() => {
       console.log(nextQuestionID);
       nextQuestionElement = $(`#${nextQuestionID}`);
       nextQuestionElement.removeClass("hidden");
+      //slow scroll to the next question
+      nextQuestionElement[0].scrollIntoView({
+        behavior: "smooth",
+      });
     }
     //Hide all child containers of non-selected buttons
     notSelectedButton.each(function () {
--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,10 +1,51 @@
-if (targetPageID === "guided-derivative-subjects-organization-tab") {
-  renderSubjectsHighLevelFolderAsideItems("derivative");
-  guidedUpdateFolderStructure("derivative", "subjects");
+/*********** Protocol page functions ***********/
+$("#guided-button-has-protocol-data").on("click", () => {
+  if (datasetStructureJSONObj["folders"]["protocol"] == undefined)
+    datasetStructureJSONObj["folders"]["protocol"] = {
+      folders: {},
+      files: {},
+      type: "",
+      action: [],
+    };
   $("#guided-file-explorer-elements").appendTo(
-    $("#guided-derivative-subjects-file-explorer-container")
+    $("#guided-user-has-protocol-data")
   );
-  updateFolderStructureUI(highLevelFolderPageData.derivative); //temp
-  document.getElementById("structure-folder-header").classList.add("hidden");
-  document.getElementById("structure-folder-contents").classList.add("hidden");
-}
+  updateFolderStructureUI(highLevelFolderPageData.protocol);
+});
+$("#guided-button-no-protocol-data").on("click", () => {
+  //ask user to confirm they would like to delete protocol folder if it exists
+  if (datasetStructureJSONObj["folders"]["protocol"] != undefined) {
+    Swal.fire({
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      title:
+        "Reverting your decision will wipe out any changes you have made to the protocol folder.",
+      text: "Are you sure you would like to delete your protocol folder progress?",
+      icon: "warning",
+      showConfirmButton: true,
+      confirmButtonText: "Delete",
+      confirmButtonColor: "#3085d6 !important",
+      showCancelButton: true,
+      focusCancel: true,
+      reverseButtons: reverseSwalButtons,
+      heightAuto: false,
+      customClass: "swal-wide",
+      backdrop: "rgba(0,0,0, 0.4)",
+      showClass: {
+        popup: "animate__animated animate__zoomIn animate__faster",
+      },
+      hideClass: {
+        popup: "animate__animated animate__zoomOut animate__faster",
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //User agrees to delete protocol folder
+        delete datasetStructureJSONObj["folders"]["protocol"];
+      } else {
+        //User cancels
+        //reset button UI to how it was before the user clicked no protocol files
+        $("#guided-button-has-protocol-data").click();
+      }
+    });
+  }
+});
