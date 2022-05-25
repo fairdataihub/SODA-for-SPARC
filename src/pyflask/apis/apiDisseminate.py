ac7307368e6b67d2613eb32c05f30c95840614aa
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-05-25 09:02:42 -0700

    feat: Get DOI from disseminate endpoint

--- src/pyflask/apis/apiDisseminate.py
+++ src/pyflask/apis/apiDisseminate.py
@@ -12,12 +12,17 @@ from namespaces import NamespaceEnum, get_namespace
 api = get_namespace(NamespaceEnum.DISSEMINATE_DATASETS)
 
 parser = api.parser()
-parser.add_argument("selected_bfaccount", type=str, help="Pennsieve account name")
-parser.add_argument("selected_bfdataset", type=str, help="Pennsieve dataset name")
+parser.add_argument("selected_bfaccount", type=str, help="Pennsieve account name", location="args")
+parser.add_argument("selected_bfdataset", type=str, help="Pennsieve dataset name", location="args")
 
 @api.route("/bf_get_doi")
 class BfGetDoi(Resource):
     @api.doc(responses={200: "Success", 400: "Validation Error", 500: "Internal Server Error"})
     @api.expect(parser)
     def get(self):
-        return bf_get_doi()
+        # get the arguments
+        args = parser.parse_args()
+        selected_bfaccount = args["selected_bfaccount"]
+        selected_bfdataset = args["selected_bfdataset"]
+
+        return bf_get_doi(selected_bfaccount, selected_bfdataset)
