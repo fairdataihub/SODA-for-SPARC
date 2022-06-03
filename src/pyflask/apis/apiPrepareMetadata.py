53d808342d6280d86ca958a3b556349d1cef6e22
Aaron Marroquin <aaron.marroquin96@gmail.com>
2022-06-02 18:12:29 -0700

    refactor(api): use reqparse in prepare_metadata routes

--- src/pyflask/apis/apiPrepareMetadata.py
+++ src/pyflask/apis/apiPrepareMetadata.py
@@ -24,20 +24,20 @@ from errorHandlers import notBadRequestException
 api = get_namespace(NamespaceEnum.PREPARE_METADATA)
 
 # upload_boolean, bfaccount, bfdataset, filepath, json_str
-parser = api.parser()
-parser.add_argument('upload_boolean', type=bool, help='Boolean to indicate whether to upload the file to the Bionimbus server', location="args")
-parser.add_argument('bfaccount', type=str, help='Bionimbus account name', location="args")
-parser.add_argument('bfdataset', type=str, help='Bionimbus dataset name', location="args")
-parser.add_argument('filepath', type=str, help='Path to the file to be uploaded', location="args")
-parser.add_argument('json_str', type=str, help='JSON string to be uploaded', location="json")
+parser_save_submission_file = reqparse.RequestParser(bundle_errors=True)
+parser_save_submission_file.add_argument('upload_boolean', type=bool, help='Boolean to indicate whether to upload the file to the Bionimbus server', location="args")
+parser_save_submission_file.add_argument('bfaccount', type=str, help='Bionimbus account name', location="args")
+parser_save_submission_file.add_argument('bfdataset', type=str, help='Bionimbus dataset name', location="args")
+parser_save_submission_file.add_argument('filepath', type=str, help='Path to the file to be uploaded', location="args")
+parser_save_submission_file.add_argument('json_str', type=str, help='JSON string to be uploaded', location="json")
 
 
 @api.route('/save_submission_file')
 class SaveSubmissionFile(Resource):
 
-    @api.expect(parser)
+    @api.expect(parser_save_submission_file)
     def get(self):
-        args = parser.parse_args()
+        args = parser_save_submission_file.parse_args()
         upload_boolean = args['upload_boolean']
         bfaccount = args['bfaccount']
         bfdataset = args['bfdataset']
@@ -48,14 +48,14 @@ class SaveSubmissionFile(Resource):
         return save_submission_file(upload_boolean, bfaccount, bfdataset, filepath, json_str)
 
 
-parser = api.parser()
-parser.add_argument('path', type=str, help='Path to the local data deliverables document', location="args")
+parser_import_milestone = reqparse.RequestParser(bundle_errors=True)
+parser_import_milestone.add_argument('path', type=str, help='Path to the local data deliverables document', location="args")
 @api.route('/import_milestone')
 class ImportMilestone(Resource):
 
-    @api.expect(parser)
+    @api.expect(parser_import_milestone)
     def get(self):
-        args = parser.parse_args()
+        args = parser_import_milestone.parse_args()
         path = args['path']
         try:
             return import_milestone(path)
