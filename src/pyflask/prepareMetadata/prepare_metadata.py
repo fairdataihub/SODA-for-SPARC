49b9b28c2869df73edf2da112c1a3e3af7ed9914
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-06-09 13:16:47 -0700

    refactor(api): compound condition to element check in list
    set_template_path in prepare_metadata.py uses list check instead of compound conditional

--- src/pyflask/prepareMetadata/prepare_metadata.py
+++ src/pyflask/prepareMetadata/prepare_metadata.py
@@ -90,11 +90,9 @@ def set_template_path(soda_base_path, soda_resources_path):
     TEMPLATE_PATH = join(soda_base_path, "file_templates")
 
     # check if os is Darwin/Linux
-    if platform.system() == "Darwin" or platform.system() == "Linux":
-        # check if the TEMPLATE_PATH exists
-        if not exists(TEMPLATE_PATH):
-            # we are in production and we need to use the Resources folder for the file_templates folder
-            TEMPLATE_PATH = join(soda_resources_path, "file_templates")
+    if platform.system() in ["Darwin", "Linux"] and not exists(TEMPLATE_PATH):
+        # we are in production and we need to use the Resources folder for the file_templates folder
+        TEMPLATE_PATH = join(soda_resources_path, "file_templates")
 
 
 # custom Exception class for when a DDD file is in an invalid form
