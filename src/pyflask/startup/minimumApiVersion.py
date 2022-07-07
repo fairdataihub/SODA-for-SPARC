da4693d91d520c07b02ac3cb72d1655ebcfb0384
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-07-07 15:57:56 -0700

    build: update the api version of the app

--- src/pyflask/startup/minimumApiVersion.py
+++ src/pyflask/startup/minimumApiVersion.py
@@ -4,4 +4,4 @@ def get_api_version():
     """
     Returns the version of the API
     """
-    return {'version': os.getenv('API_VERSION', "6.0.0")}
+    return {'version': os.getenv('API_VERSION', "7.0.0")}
