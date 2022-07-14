7dbb4f485aa28509565a9a558b45b4b9d7531bda
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-07-14 13:45:14 -0700

    refactor: insert HTML into DOM using fetch and templates
    Previously this was handled in index.html by using a link tage and providing a rel=import attribute. However that has been deprecated.

--- src/assets/imports.js
+++ src/assets/imports.js
@@ -1,12 +1,40 @@
-const links = document.querySelectorAll('link[rel="import"]');
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}   
 
-// Import and add each page to the DOM
-Array.prototype.forEach.call(links, (link) => {
-  let template = link.import.querySelector(".task-template");
-  let clone = document.importNode(template.content, true);
-  //if (link.href.match('about.html')) {
-  //  document.querySelector('body').appendChild(clone)
-  //} else {
-  document.querySelector(".content").appendChild(clone);
-  //}
+/**
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+ function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  console.log(template)
+  return template.content
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  const links = document.querySelectorAll('link[rel="import"]')
+
+  // Import and add each page to the DOM
+  Array.prototype.forEach.call(links, async (link) => {
+    let doc = await fetch(link.href, {
+      headers: {
+        'Content-Type': 'text/html'
+      }
+    })
+    let content = await doc.text()
+    let template = htmlToElement(content)
+    let clone = document.importNode(template, true);
+    document.querySelector("#content").appendChild(clone);
+  });
+
+
 });
