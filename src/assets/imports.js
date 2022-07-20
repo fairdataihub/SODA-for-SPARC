0c8bb9a163312dfeb5897dc8f0765f6a1744d62f
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-07-20 12:28:54 -0700

    feat: prepare metadata section functional (mostly)

--- src/assets/imports.js
+++ src/assets/imports.js
@@ -63,19 +63,77 @@ const insertScript = async () => {
   await ws(500)
 
 
+  const disseminate = document.createElement("script");
+  disseminate.src = "./scripts/disseminate/disseminate.js";
+  disseminate.defer = true;
+  disseminate.type = "text/javascript";
+  document.body.appendChild(disseminate);
+
+  await ws(500);
+
   const prePublishingReview = document.createElement("script");
   prePublishingReview.src = "./scripts/disseminate/prePublishingReview.js";
   prePublishingReview.defer = true;
   prePublishingReview.type = "text/javascript";
   document.body.appendChild(prePublishingReview);
 
-  await ws(500);
+  await ws(500)
 
   const manageDatasets = document.createElement("script");
   manageDatasets.src = "./scripts/manage-dataset/manage-dataset.js";
   manageDatasets.defer = true;
   manageDatasets.type = "text/javascript";
   document.body.appendChild(manageDatasets);
+
+  await ws(500)
+
+  const datasetDescription = document.createElement("script");
+  datasetDescription.src = "./scripts/metadata-files/datasetDescription.js";
+  datasetDescription.defer = true;
+  datasetDescription.type = "text/javascript";
+  document.body.appendChild(datasetDescription);
+
+  await ws(500)
+
+  const curateFunctions = document.createElement("script");
+  curateFunctions.src = "./scripts/organize-dataset/curate-functions.js";
+  curateFunctions.defer = true;
+  curateFunctions.type = "text/javascript";
+  document.body.appendChild(curateFunctions);
+
+  await ws(500)
+
+  const manifest = document.createElement("script");
+  manifest.src = "./scripts/metadata-files/manifest.js";
+  manifest.defer = true;
+  manifest.type = "text/javascript";
+  document.body.appendChild(manifest);
+
+  await ws(500)
+
+  const readmeChanges = document.createElement("script");
+  readmeChanges.src = "./scripts/metadata-files/readme-changes.js";
+  readmeChanges.defer = true;
+  readmeChanges.type = "text/javascript";
+  document.body.appendChild(readmeChanges);
+
+  await ws(500)
+
+  const subjectsSamples = document.createElement("script");
+  subjectsSamples.src = "./scripts/metadata-files/subjects-samples.js";
+  subjectsSamples.defer = true;
+  subjectsSamples.type = "text/javascript";
+  document.body.appendChild(subjectsSamples);
+
+  await ws(500)
+
+  const submission = document.createElement("script");
+  submission.src = "./scripts/metadata-files/submission.js";
+  submission.defer = true;
+  submission.type = "text/javascript";
+  document.body.appendChild(submission);
+
+
 };
 
 const ws = (ms) => {
