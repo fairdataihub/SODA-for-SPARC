4c1132ae0b50d3bfbd113ea306e31c17593c753e
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-08-05 14:52:23 -0700

    feat: added story for soda header:

--- src/stories/SodaHeader.stories.js
+++ src/stories/SodaHeader.stories.js
@@ -1,2 +1,22 @@
 const SodaHeader = require("./SodaHeader")
 
+export default {
+    headerText: "Soda Header",
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
+        layout: "fullscreen",
+
+    }
+};
+
+const Template = (args) => SodaHeader(args);
+
+export const story = Template.bind({})
+
+let argumentObject = {
+    headerText: "Manage Datasets - Add/edit subtitle",
+    needHelpLink: "https://docs.sodaforsparc.io/docs/manage-dataset/add-edit-subtitle"
+}
+
+story.args = argumentObject
+
