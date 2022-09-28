c46fa80b78095ecdfeafed3a2a4011376869ef1c
slugb0t <wheresdorian@gmail.com>
2022-09-28 14:27:36 -0700

    refactor: updated version number

--- pyflask/startup/minimumApiVersion.py
+++ pyflask/startup/minimumApiVersion.py
@@ -4,5 +4,5 @@ def get_api_version():
     """
     Returns the version of the API
     """
-    return {'version': os.getenv('API_VERSION', "9.2.1")}
+    return {'version': os.getenv('API_VERSION', "9.2.2")}
 
