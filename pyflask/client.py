3bb4b587007fd1c5a8854c1cb3e6aa2568ffb4d5
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-09-29 12:34:44 -0700

    build: add pennsieve to conda environment

--- pyflask/client.py
+++ pyflask/client.py
@@ -4,10 +4,10 @@ from os import path
 client = Pennsieve()
 client.useDataset("N:dataset:1cb4bf59-2b6d-48c9-8dae-88f722c6e328")
 
-manifest_manager = client.manifest 
 
-local_ds = path.join(expanduser("~"), "Desktop", "tessst")
-created_manifest = manifest_manager.create(local_ds)
+
+local_ds = path.join(path.expanduser("~"), "Desktop", "tessst")
+created_manifest = client.manifest.create(local_ds)
 
 print(created_manifest)
 
