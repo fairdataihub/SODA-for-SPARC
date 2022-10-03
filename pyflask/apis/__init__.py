fec5c50764d30396cef624a5f83eae4dd827ef3a
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-10-03 14:46:58 -0700

    feat: echo and minapiversion routes

--- pyflask/apis/__init__.py
+++ pyflask/apis/__init__.py
@@ -5,7 +5,7 @@ from .apiManageDatasets import api as manage_datasets_resource
 # from .apiDisseminate import api as disseminate_datasets_resource
 # from .apiPrepareMetadata import api as prepare_metadata_resource
 # from .apiOrganizeDatasets import api as organize_datasets_resource
-# from .apiStartup import api as startup_resource
+from .apiStartup import api as startup_resource
 # from .apiTaxonomy import api as taxonomy_resource
 # from .apiUser import api as user_resource
 # from .apiDatasets import api as datasets_resource
