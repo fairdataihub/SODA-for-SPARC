e1f4be38db456554124ea2867e80cc9777a157fe
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-10-06 09:47:46 -0700

    feat: basic way to track amount of files uploaded with subscriber

--- pyflask/client.py
+++ pyflask/client.py
@@ -8,16 +8,31 @@ client.useDataset("N:dataset:1cb4bf59-2b6d-48c9-8dae-88f722c6e328")
 
 
 
-local_ds = path.join(path.expanduser("~"), "Desktop", "9k-files-gened")
-manifest = client.manifest.create(local_ds)
-
-print(manifest)
-print(type(manifest))
-
-for s in manifest:
-    print(s)
+# local_ds = path.join(path.expanduser("~"), "Desktop", "9k-files-gened")
+# manifest = client.manifest.create(local_ds)
 
 
 
-#sub = client.subscribe(2)
-# client.manifest.upload(2)
+client.manifest.upload(5)
+sub = client.subscribe(10)
+
+counter = 0 
+end_counter = 1000
+msgs = []
+print(dir(sub))
+for msg in sub:
+    if msg.upload_status.total == msg.upload_status.current:
+        print("Finished")
+        counter += 1
+        print(counter)
+    
+
+print("Done")
+
+
+
+client.unsubscribe(10)
+print("Client unsubscribed")
+
+
+
