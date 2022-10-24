560f6e79eb459262843629345b8fcd6afc324c03
slugb0t <wheresdorian@gmail.com>
2022-10-24 12:30:07 -0700

    converted apiDatasets to new pennsieve agent

--- pyflask/datasets/__init__.py
+++ pyflask/datasets/__init__.py
@@ -1,7 +1,7 @@
 from .datasets import ( 
-    # get_role, 
-    # get_dataset_by_id,
+    get_role, 
+    get_dataset_by_id,
     get_current_collection_names, 
-    # upload_collection_names,
-    # remove_collection_names
+    upload_collection_names,
+    remove_collection_names
 )
