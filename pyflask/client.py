d56cc37fda48c69525eada171ec4e5f85a411235
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-10-25 14:54:39 -0700

    feat: check agent version and start agent ( linux )

--- pyflask/client.py
+++ pyflask/client.py
@@ -1,18 +1,22 @@
 from pennsieve2.pennsieve import Pennsieve
 from os import path
 import requests
+import sys 
+import subprocess
+from subprocess import STDOUT
+from os.path import exists 
 
-client = Pennsieve()
-client.user.switch('sodie')
-client.user.reauthenticate()
-client.useDataset("N:dataset:1cb4bf59-2b6d-48c9-8dae-88f722c6e328")
+# client = Pennsieve()
+# client.user.switch('sodie')
+# client.user.reauthenticate()
+# client.useDataset("N:dataset:1cb4bf59-2b6d-48c9-8dae-88f722c6e328")
 
 
 
 #local_ds = path.join(path.expanduser("~"), "Desktop", "DatasetTemplate")
-manifest = client.manifest.create(local_ds)
+# manifest = client.manifest.create(local_ds)
 
-PENNSIEVE_URL = "https://api.pennsieve.io"
+# PENNSIEVE_URL = "https://api.pennsieve.io"
 
 # try:
 #   r = requests.post(f"{PENNSIEVE_URL}/packages", headers={"Content-Type": "application/json", "Authorization": f"Bearer {client.getUser()['session_token']}",}, 
@@ -62,5 +66,93 @@ PENNSIEVE_URL = "https://api.pennsieve.io"
 
 # print("Done")
 
+
+
+
+
+
+def get_agent_installation_location():
+    """
+        Get the location of the Pennsieve agent installation for Darwin, Linux, and Windows. 
+    """
+    if sys.platform == "darwin":
+        return "/usr/local/opt/pennsieve/bin/pennsieve"
+
+    elif sys.platform.startswith("linux"):
+        return "/usr/local/bin/pennsieve"
+
+    elif sys.platform in ["win32", "cygwin"]:
+        return "C:/Program Files/Pennsieve/pennsieve.exe"
+
+def check_agent_installation():
+    """
+    Check if the Pennsieve agent is installed on the computer. 
+    """
+    return exists(get_agent_installation_location())
+        
+
+
+def start_agent():
+    """
+    Start the Pennsieve agent. IMP: Run if agent exists.
+    """
+    if not check_agent_installation(): 
+        raise FileNotFoundError("Pennsieve agent not installed. Please install the agent before running this function.")
+    
+    command = [get_agent_installation_location(), "agent"]
+
+    return subprocess.run(command, check=True)
+
+
+def get_agent_version():
+    """
+        Get the version of the Pennsieve agent installed on the computer.
+    """
+    if not check_agent_installation(): 
+        raise FileNotFoundError("Pennsieve agent not installed. Please install the agent before running this function.")
+    
+    try:
+        version = subprocess.run([get_agent_installation_location(), "version"], capture_output=True, check=True).stdout
+    except Exception as e:
+        raise e
+    
+    
+    # decode the response 
+    version = version.decode().strip()
+
+    return version
+
+
+def agent_up_to_date():
+    v = get_agent_version()
+
+    # search string for 1.2.2
+    # TODO: Improve agent version parsing to check for Agent Version and CLI Version separately. Both need to match.
+    if "1.2.2" in v:
+        print("Agent is up to date")
+        return True
+    else:
+        print("Agent is not up to date")
+        return False
+
+
+# if check_agent_installation():
+#     start_agent()
+
+
+#     if not agent_up_to_date():
+#         print("Please update the agent to the latest version.") # provide a link to the latest version of the agent
+#     else:
+#         print("Continuing to use the agent")
+
+# else:
+#     print("Agent not installed")
+
+try:
+    get_agent_version()
+except Exception as e:
+    print(e)
+
+    
 
 
--- pyflask/pysodaUtils/pysodaUtils.py
+++ pyflask/pysodaUtils/pysodaUtils.py
@@ -9,6 +9,7 @@ from websocket import create_connection
 import socket
 import errno
 import re
+import sys
 
 
 def clear_queue():
@@ -37,6 +38,58 @@ def agent_running():
         raise AgentError(
             "The Pennsieve agent is already running. Learn more about how to solve the issue <a href='https://docs.sodaforsparc.io/docs/common-errors/pennsieve-agent-is-already-running' target='_blank'>here</a>."
         )
+
+
+
+def get_agent_installation_location():
+    """
+        Get the location of the Pennsieve agent installation for Darwin, Linux, and Windows. 
+    """
+    if sys.platform == "darwin":
+        return "/usr/local/opt/pennsieve/bin/pennsieve"
+
+    elif sys.platform.startswith("linux"):
+        return "/usr/local/bin/pennsieve"
+
+    elif sys.platform in ["win32", "cygwin"]:
+        return "C:/Program Files/Pennsieve/pennsieve.exe"
+
+def check_agent_installation():
+    """
+    Check if the Pennsieve agent is installed on the computer. 
+    """
+    return exists(get_agent_installation_location())
+        
+
+
+def start_agent():
+    """
+    Start the Pennsieve agent. IMP: Run if agent exists.
+    """
+    if not check_agent_installation(): 
+        raise FileNotFoundError("Pennsieve agent not installed. Please install the agent before running this function.")
+    
+    return subprocess.run(get_agent_installation_location(), check=True)
+
+
+def get_agent_version():
+    """
+        Get the version of the Pennsieve agent installed on the computer.
+    """
+    if not check_agent_installation(): 
+        raise FileNotFoundError("Pennsieve agent not installed. Please install the agent before running this function.")
+    
+    try:
+        version = subprocess.check_output([get_agent_installation_location(), "version"], check=True, stderr=STDOUT)
+    except Exception as e:
+        raise e
+    
+    
+    # decode the response 
+    version = version.decode().strip()
+
+    return version
+
 
 
 forbidden_characters = '<>:"/\|?*'
