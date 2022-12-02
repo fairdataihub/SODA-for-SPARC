9e34cdf739e2ebc1ca1b45687280546b69cabb2b
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-12-02 10:49:19 -0800

    fix: main_curate_function_upload_Details undefined

--- pyflask/apis/apiCurate.py
+++ pyflask/apis/apiCurate.py
@@ -11,7 +11,6 @@ from curate import (
     generate_manifest_file_locally,
     guided_generate_manifest_file_data,
     check_JSON_size,
-    main_curate_function_upload_details,
     create_high_level_manifest_files_existing_local_starting_point,
 )
 from errorHandlers.notBadRequestException import notBadRequestException
@@ -115,16 +114,6 @@ model_curation_file_details_response = api.model( "CurationFileDetailsResponse",
     "generated_dataset_id": fields.String(description="ID of the dataset that has been generated. ")
 })
 
-@api.route("/curation/upload_details")
-class CurationFileDetails(Resource):
-    
-        @api.marshal_with(model_curation_file_details_response, False, 200)
-        @api.doc(responses={500: 'There was an internal server error'}, description="Function frequently called by front end to help keep track of the amount of files that have been successfully uploaded to Pennsieve, and the size of the uploaded files. Also tells us how many files have been copied (double usage of both variables) to a destination folder for local dataset generation.")
-        def get(self):
-            try:
-                return main_curate_function_upload_details()
-            except Exception as e:
-                api.abort(500, str(e))
 
 
 
