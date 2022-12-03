fcbe98bd0318065ddff35026bfea5f8676f3d7c0
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-12-02 17:13:09 -0800

    chore: console logs for tracking issue with startup flow

--- preload.js
+++ preload.js
@@ -371,11 +371,13 @@ const openDropdownPrompt = async (ev, dropdown, show_timer = true) => {
           let apiSecret = result.value.secret;
           //needs to be replaced
           try {
+            console.log("Sending request to add account");
             await client.put(`/manage_datasets/account/username`, {
               keyname: key_name,
               key: apiKey,
               secret: apiSecret,
             });
+            console.log("Added the account. Now getting the account list");
             bfAccountOptions[key_name] = key_name;
             defaultBfAccount = key_name;
             defaultBfDataset = "Select dataset";
