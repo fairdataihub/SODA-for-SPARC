b7da2c97f5e77cff255d5c57addbfa88f046865a
aaronm-2112 <aaron.marroquin96@gmail.com>
2022-12-05 10:01:55 -0800

    refactor: use api to get access token instead of client - collections

--- pyflask/client.py
+++ pyflask/client.py
@@ -6,12 +6,60 @@ import subprocess
 from subprocess import STDOUT
 from os.path import exists 
 
-client = Pennsieve()
-client.user.switch('soda')
-client.user.reauthenticate()
+import boto3
+import requests
+from os.path import expanduser, join
+from configparser import ConfigParser
+
+userpath = expanduser("~")
+configpath = join(userpath, ".pennsieve", "config.ini")
+PENNSIEVE_URL = "https://api.pennsieve.io"
+
+def read_from_config(key):
+    config = ConfigParser()
+    config.read(configpath)
+    if "global" not in config:
+        raise Exception("Profile has not been set")
+
+    keyname = config["global"]["default_profile"]
+
+    if keyname in config and key in config[keyname]:
+        return config[keyname][key]
+    return None
+
+def get_access_token():
+    # get cognito config 
+    r = requests.get(f"{PENNSIEVE_URL}/authentication/cognito-config")
+    r.raise_for_status()
+
+    cognito_app_client_id = r.json()["tokenPool"]["appClientId"]
+    cognito_region_name = r.json()["region"]
+
+    cognito_idp_client = boto3.client(
+    "cognito-idp",
+    region_name=cognito_region_name,
+    aws_access_key_id="",
+    aws_secret_access_key="",
+    )
+            
+    login_response = cognito_idp_client.initiate_auth(
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": read_from_config("api_token"), "PASSWORD": read_from_config("api_secret")},
+    ClientId=cognito_app_client_id,
+    )
+        
+    return login_response["AuthenticationResult"]["AccessToken"]
+
+print(get_access_token())
+
+# client = Pennsieve()
+# client.user.switch('soda')
+# client.user.reauthenticate()
 # client.useDataset("N:dataset:8a2d765f-fa97-4e76-b534-deedb6757571")
 
 PENNSIEVE_URL = "https://api.pennsieve.io"
+
+
 
 def create_request_headers(ps):
     """
--- pyflask/collectionsDataset/collectionsDataset.py
+++ pyflask/collectionsDataset/collectionsDataset.py
@@ -2,7 +2,7 @@ from flask import abort
 import requests 
 from permissions import bf_get_current_user_permission_agent_two, has_edit_permissions
 from utils import get_dataset_id, connect_pennsieve_client, authenticate_user_with_client, create_request_headers
-
+from authentication import get_access_token
 
 PENNSIEVE_URL = "https://api.pennsieve.io"
 
@@ -11,11 +11,9 @@ def get_all_collections(account):
     Function used to get the collections that belong to an organization
     """
 
-    ps = connect_pennsieve_client()
+    token = get_access_token()
 
-    authenticate_user_with_client(ps, account)
-
-    r = requests.get(f"{PENNSIEVE_URL}/collections", headers=create_request_headers(ps))
+    r = requests.get(f"{PENNSIEVE_URL}/collections", headers=create_request_headers(token))
     r.raise_for_status()
 
     return r.json()
@@ -31,19 +29,17 @@ def upload_new_names(account, dataset, tags):
 
     statusResponses = []
 
-    ps = connect_pennsieve_client()
+    token = get_access_token()
 
-    authenticate_user_with_client(ps, account)
+    selected_dataset_id = get_dataset_id(token, dataset)
 
-    selected_dataset_id = get_dataset_id(ps, dataset)
-
-    if not has_edit_permissions(ps, selected_dataset_id):
+    if not has_edit_permissions(token, selected_dataset_id):
         abort(403, "You do not have permission to edit this dataset.")
 
 
     for tag in tags:
         jsonfile = {"name": tag}
-        r = requests.post(f"{PENNSIEVE_URL}/collections", headers=create_request_headers(ps) ,json=jsonfile)
+        r = requests.post(f"{PENNSIEVE_URL}/collections", headers=create_request_headers(token) ,json=jsonfile)
         r.raise_for_status()
         result = r.json()
         statusResponses.append(result)
