c6554b71c79f287ef4a349eae8ac979375f3a9a0
Jacob Clark <jacobcs@zoho.com>
2023-02-01 17:04:21 -0800

    fix: 🐛 add all verify false

--- pyflask/organizeDatasets/organize_datasets.py
+++ pyflask/organizeDatasets/organize_datasets.py
@@ -1188,7 +1188,7 @@ def import_pennsieve_dataset(soda_json_structure, requested_sparc_only=True):
             collection_id = soda_json_structure["dataset-structure"]["folders"][folder][
                 "path"
             ]
-            r = requests.get(f"{PENNSIEVE_URL}/packages/{collection_id}", headers=headers)
+            r = requests.get(f"{PENNSIEVE_URL}/packages/{collection_id}", headers=headers, verify=False)
             r.raise_for_status()
             subfolder = r.json()
 
@@ -1201,12 +1201,12 @@ def import_pennsieve_dataset(soda_json_structure, requested_sparc_only=True):
                     package_id = items["content"]["id"]
                     if package_name in manifest_sparc:
                         # item is manifest
-                        r = requests.get(f"{PENNSIEVE_URL}/packages/{package_id}/view", headers=headers)
+                        r = requests.get(f"{PENNSIEVE_URL}/packages/{package_id}/view", headers=headers, verify=False)
                         r.raise_for_status()
                         file_details = r.json()
 
                         file_id = file_details[0]["content"]["id"]
-                        r = requests.get(f"{PENNSIEVE_URL}/packages/{package_id}/files/{file_id}", headers=headers)
+                        r = requests.get(f"{PENNSIEVE_URL}/packages/{package_id}/files/{file_id}", headers=headers, verify=False)
                         r.raise_for_status()
                         manifest_url = r.json()["url"]
 
