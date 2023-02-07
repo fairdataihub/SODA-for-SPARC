9177403ed33cecf5f848b5acc807fbc53b50a854
aaronm-2112 <aaron.marroquin96@gmail.com>
2023-02-07 10:53:56 -0800

    chore: resolve merge issues

--- pyflask/organizeDatasets/organize_datasets.py
+++ pyflask/organizeDatasets/organize_datasets.py
@@ -1143,12 +1143,12 @@ def import_pennsieve_dataset(soda_json_structure, requested_sparc_only=True):
 
     # root of dataset is pulled here
     # root_children is the files and folders within root
-    r = requests.get(f"{PENNSIEVE_URL}/datasets/{selected_dataset_id}", headers=headers, verify=False)
+    r = requests.get(f"{PENNSIEVE_URL}/datasets/{selected_dataset_id}", headers=headers)
     r.raise_for_status()
     root_folder = r.json()
 
     # root's packages 
-    r = requests.get(f"{PENNSIEVE_URL}/datasets/{selected_dataset_id}/packageTypeCounts", headers=headers, verify=False)
+    r = requests.get(f"{PENNSIEVE_URL}/datasets/{selected_dataset_id}/packageTypeCounts", headers=headers)
     r.raise_for_status()
     packages_list = r.json()
 
