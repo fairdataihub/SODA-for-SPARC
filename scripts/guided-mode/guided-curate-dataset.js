68e7c80456b6f917be23cd3707e33e894e34451d
slugb0t <wheresdorian@gmail.com>
2023-03-29 16:46:49 -0700

    fix: 🐛 auto generate manifest fix, styling fixes and other fixes

--- scripts/guided-mode/guided-curate-dataset.js
+++ scripts/guided-mode/guided-curate-dataset.js
@@ -2816,8 +2816,10 @@ document
       );
       let response = cleanJson.data.soda_json_structure;
       // response does not format in JSON format so need to format ' with "
+      // and replace T with t (happens because of how the bool true is formatted in python (True) vs javascript (true))
       let regex = /'/gm;
-      let formattedResponse = response.replace(regex, '"');
+      console.log(response);    
+      let formattedResponse = JSON.stringify(response).replace(regex, '"');
       let capitalTPosition = formattedResponse.search("T");
       while (capitalTPosition != -1) {
         capitalTPosition = formattedResponse.search("T");
