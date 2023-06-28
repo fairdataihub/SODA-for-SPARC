e798ee4b8287761f666dbbbb051788cb24c39715
slugb0t <wheresdorian@gmail.com>
2023-06-28 11:36:16 -0700

    fix: 🐛 determineDatasetLocation sends correct location for all cases

--- scripts/others/analytics/analytics-utils.js
+++ scripts/others/analytics/analytics-utils.js
@@ -3,29 +3,21 @@ const determineDatasetLocation = () => {
   let location = "";
   let datasetLocation = sodaJSONObj?.["starting-point"]?.["type"];
   console.log("datasetLocation: ", datasetLocation);
-  if("save-progress" in sodaJSONObj) {
+  if ("save-progress" in sodaJSONObj) {
     return Destinations.SAVED;
   }
   if (datasetLocation === "bf") {
     return Destinations.PENNSIEVE;
   }
-  if (datasetLocation === "local" || datasetLocation === "Local") {
+  if (
+    datasetLocation === "local" ||
+    datasetLocation === "Local" ||
+    "local-path" in sodaJSONObj?.["starting-point"]
+  ) {
     return Destinations.LOCAL;
   }
-
-  // determine if we are using a local or Pennsieve dataset
-  // This is checking for destination no? Not starting point
-  // Above if statements seem to return before this is reached though
-  if ("bf-dataset-selected" in sodaJSONObj) {
-    location = Destinations.PENNSIEVE;
-  } else if (sodaJSONObj?.["generate-dataset"]?.["destination"] != undefined){
-    location = sodaJSONObj?.["generate-dataset"]?.["destination"].toUpperCase();
-    if (location === "LOCAL") {
-      location = Destinations.LOCAL;
-    }
-    if (location === "PENNSIEVE") {
-      location = Destinations.SAVED;
-    }
+  if (datasetLocation === "new") {
+    return Destinations.NEW;
   }
 
   return location;
