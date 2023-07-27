860142903c191ccbfdb1ec1197caebe5524d5569
slugb0t <wheresdorian@gmail.com>
2023-07-27 13:57:56 -0700

    feat: ✨ kombucha event, added dataset to collection fail, ffm

--- scripts/collections/collections.js
+++ scripts/collections/collections.js
@@ -150,7 +150,7 @@ $(document).ready(function () {
           heightAuto: false,
           backdrop: "rgba(0,0,0, 0.4)",
         });
-
+        
         ipcRenderer.send(
           "track-kombucha",
           kombuchaEnums.Category.MANAGE_DATASETS,
@@ -172,6 +172,19 @@ $(document).ready(function () {
           heightAuto: false,
           backdrop: "rgba(0,0,0, 0.4)",
         });
+        
+        ipcRenderer.send(
+          "track-kombucha",
+          kombuchaEnums.Category.MANAGE_DATASETS,
+          kombuchaEnums.Action.ADD_EDIT_DATASET_METADATA,
+          kombuchaEnums.Label.COLLECTIONS,
+          kombuchaEnums.Status.FAIL,
+          {
+            value: 1,
+            dataset_name: defaultBfDataset,
+            dataset_id: defaultBfDatasetId,
+          }
+        );
       }
 
       log.info(`Adding dataset '${selectedDataset}' to Collection'`);
