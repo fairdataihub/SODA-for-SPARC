8cd9efbc63d1656f7beb547ae3b5127974ed9664
aaronm-2112 <aaron.marroquin96@gmail.com>
2023-12-04 14:02:41 -0800

    fix: auto updater launching twice

--- src/main/index.js
+++ src/main/index.js
@@ -533,11 +533,7 @@ const initialize = () => {
     // show the splash screen
 
 
-    app.on('activate', function () {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+ 
 
     // track app launch at Kombucha analytics server
     // trackKombuchaEvent(
