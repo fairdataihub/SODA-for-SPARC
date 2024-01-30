b5ece4b5fffeb6a1e0a778078e875497d58c596b
Jacob Clark <jacobcs@zoho.com>
2024-01-30 12:31:12 -0800

    wip: Added mfplus python package

--- src/pyflask/startup/minimumApiVersion.py
+++ src/pyflask/startup/minimumApiVersion.py
@@ -4,4 +4,4 @@ def get_api_version():
     """
     Returns the version of the API
     """
-    return {'version': os.getenv('API_VERSION', "13.1.1-beta")}
+    return {'version': os.getenv('API_VERSION', "13.2.2-beta")}
