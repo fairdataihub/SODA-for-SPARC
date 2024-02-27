cd64428c9f669c0d62f37d63f597ed0f309d86ad
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-02-27 10:34:45 -0800

    wip: unit test

--- src/renderer/tests/test.jsx
+++ src/renderer/tests/test.jsx
@@ -1,4 +1,5 @@
 import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
 import Link from "./test-link";
 import React from "react";
 
@@ -6,7 +7,7 @@ test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-it("changes the class when hovered", () => {
+it("is an example of a snapshot test using react-test-renderer", () => {
   const component = renderer.create(<Link page="http://www.facebook.com">Facebook</Link>);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
@@ -26,4 +27,21 @@ it("changes the class when hovered", () => {
   // re-rendering
   tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it("is an example of a unit test of the Link component", () => {
+  const component = render(<Link page="http://www.facebook.com">Facebook</Link>);
+  const linkElement = component.getByText("Facebook");
+  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toHaveClass("normal");
+
+  renderer.act(() => {
+    component.getByText("Facebook").onMouseEnter();
+  });
+  expect(linkElement).toHaveClass("hovered");
+
+  renderer.act(() => {
+    component.getByText("Facebook").onMouseLeave();
+  });
+  expect(linkElement).toHaveClass("normal");
 });
