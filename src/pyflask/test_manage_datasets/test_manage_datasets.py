9632d4bdfc6fa4cdee3eff64835a66ad65f1b159
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-03-13 11:46:31 -0700

    chore: add comments to explain test procedure

--- src/pyflask/test_manage_datasets/test_manage_datasets.py
+++ src/pyflask/test_manage_datasets/test_manage_datasets.py
@@ -1,11 +1,16 @@
+# Run the test with the following command from src/pyflask: pytest -v
+
+# namespaces are not configured in the manageDatasets module so configure them now before the test
 from namespaces import configure_namespaces
 configure_namespaces()
 
+# import the function(s) to test
 from manageDatasets import get_number_of_files_and_folders_locally 
 
 class TestManageDatasets:
-
+    # utilize the tmp_path fixture to create a temporary directory that is reset each test
     def test_get_number_of_files_and_folders_locally_empty_dir(tmp_path):
+        # use pytests assert library
         assert get_number_of_files_and_folders_locally(str(tmp_path)) == {
             "totalFiles": 0, 
             "totalDir": 0
