2434aff4cd88f74db1c0d2a98dcfa04c5d105f26
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-04-04 17:41:40 -0700

    refactor: simplify manifest session class

--- src/pyflask/curate/manifestSession/manifest_session.py
+++ src/pyflask/curate/manifestSession/manifest_session.py
@@ -8,64 +8,26 @@ class UploadManifestSession:
 
     # properties 
     df_mid = None
-    mdf_mid = None
-    mff_mid = None
     ps = None
 
     # upload values 
     main_total_generate_dataset_size = None
-    total_bytes_uploaded = {"value": 0} 
-    bytes_uploaded_per_file = None # if the agent stops this needs to be stored in order to get an accurate accounting of what we had vs how much is now uplaoded for each file 
-    total_files_uploaded = None
     total_files_to_upload = None 
-    completed_files_byte_count = {"value": 0}
 
     def __init__(self):
         self.df_mid = None
-        self.mdf_mid = None
-        self.mff_mid = None
 
     def set_df_mid(self, id):
         self.df_mid = id
-
-    def set_mdf_mid(self, id):
-        self.mdf_mid = id
-    
-    def set_mff_mid(self, id):
-        self.mff_mid = id
     
     def get_df_mid(self):
         return self.df_mid
-    
-    def get_mdf_mid(self):
-        return self.mdf_mid
-    
-    def get_mff_mid(self):
-        return self.mff_mid
-    
-    def set_bytes_uploaded_per_file(self, size):
-        self.bytes_uploaded_per_file = size
-
-    def get_bytes_uploaded_per_file(self):
-        return self.bytes_uploaded_per_file
     
     def set_main_total_generate_dataset_size(self, size):
         self.main_total_generate_dataset_size = size
 
     def get_main_total_generate_dataset_size(self):
         return self.main_total_generate_dataset_size
-    
-    def set_total_uploaded_bytes(self, size):
-        self.total_bytes_uploaded["value"] = size
-
-    def get_total_uploaded_bytes(self):
-        return self.total_bytes_uploaded["value"]
-    
-    def set_total_files_uploaded(self, count):
-        self.total_files_uploaded = count
-
-    def get_total_files_uploaded(self):
-        return self.total_files_uploaded
     
     def set_total_files_to_upload(self, count):
         self.total_files_to_upload = count
@@ -78,18 +40,6 @@ class UploadManifestSession:
     
     def df_mid_has_progress(self):
         return self.manifest_has_progress(self.df_mid)
-    
-    def mdf_mid_has_progress(self):
-        return self.manifest_has_progress(self.mdf_mid)
-    
-    def mff_mid_has_progress(self):
-        return self.manifest_has_progress(self.mff_mid)
-    
-    def set_completed_files_byte_count(self, count):
-        self.completed_files_byte_count["value"] = count
-
-    def get_completed_files_byte_count(self):
-        return self.completed_files_byte_count["value"]
     
     def manifest_has_progress(self, mid):
         if self.ps is None:
@@ -104,12 +54,6 @@ class UploadManifestSession:
     
     def get_remaining_df_file_count(self):
         return self.get_remaining_file_count(self.df_mid)
-    
-    def get_remaining_mdf_file_count(self):
-        return self.get_remaining_file_count(self.mdf_mid)
-    
-    def get_remaining_mff_file_count(self):
-        return self.get_remaining_file_count(self.mff_mid)
     
     def get_remaining_file_count(self, mid, total_files):
         if self.ps is None:
