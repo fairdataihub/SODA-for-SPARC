4bf212a4bf1aafdc36438e05672655f1171e7e7c
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-04-11 10:59:37 -0700

    chore: removed unused tes.js file

--- src/renderer/src/scripts/others/test.js
+++
@@ -1,7 +0,0 @@
-let str = "Hello<br>World<br>How are you?";
-let replacement = "Replacement string<br>";
-
-// Replace everything up to and including the second <br>
-str = str.replace(/(.*?<br>.*?)(<br>)/, replacement);
-
-console.log(str); // Outputs: Replacement stringHow are you?
