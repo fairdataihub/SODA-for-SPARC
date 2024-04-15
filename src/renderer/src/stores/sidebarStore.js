ad816424d79784a66cf5ab1cce362dea69ae00a9
Jacob Clark <jacobcs@zoho.com>
2024-04-15 16:11:40 -0700

    wip: Remove old format stores

--- src/renderer/src/stores/sidebarStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const uiStore = create((set) => ({
-  isSidebarOpen: true,
-  setIsSidebarOpen: (isSidebarOpen) => set({ isSidebarOpen: isSidebarOpen }),
-}));
-
-export default uiStore;
