1a70fd1b6dc0a98eac68ef2c3f727c72ee8e8e3b
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-04-25 11:16:57 -0700

    refactor: paginate through manifest without file count

--- src/pyflask/uploadManifests/upload_manifests.py
+++ src/pyflask/uploadManifests/upload_manifests.py
@@ -17,16 +17,21 @@ def get_verified_files_count(manifest_id, total_files):
 
     ps = Pennsieve()
     ps.manifest.sync(manifest_id)
-    total_pages = math.ceil(total_files / 1000)
-    remaining_files = 0
+
     offset = 0
-    for i in range(total_pages):
-        if i >= 1:
-            offset += 1000
-        file_page = ps.manifest.list_files(manifest_id, offset , 1000)
-        # if there is no node_id then an upload hasn't started yet - all files are remaining 
-        # regular expression that searches and counts for every string that has "status: LOCAL" or "status: REGISTERED" in the string
-        remaining_files +=  len(re.findall(r'status: VERIFIED|status: FAILED|status: FINALIZED' , str(file_page)))
+    limit = 1000
+    while True:
+        file_page = ps.manifest.list_files(manifest_id, offset, limit)
+        # regular expression that searches and counts for every string that has "status: VERIFIED|status: FAILED|status: FINALIZED" in the string
+        new_files = len(re.findall(r'status: VERIFIED|status: FAILED|status: FINALIZED', str(file_page)))
+        remaining_files += new_files
+
+        # If the number of files returned is less than the limit, we've reached the last page
+        if new_files < limit:
+            break
+
+        # Otherwise, increment the offset by the limit and continue to the next page
+        offset += limit
 
     return remaining_files
 
