2162ac6b95afc8fcb6f65143a51ec718abc8e4a1
Jacob Clark <jacobcs@zoho.com>
2024-05-02 12:42:31 -0700

    refactor: Clarified dom loading functionality

--- src/renderer/src/assets/imports.js
+++ src/renderer/src/assets/imports.js
@@ -1,14 +1,53 @@
 import addDatasetAndOrganizationCardComponents from "../assets/component-utils/addDatasetAndOrganizationCards";
 
-// adds the apps HTML pages to the DOM
+// Declare variables used by js scripts to see if the:
+// 1. htmlSectionsAdded: When the seperate sections in HTML files have been rendered into the DOM
+// 2. baseHtmlLoaded: When the base HTML (including React components) has been rendered into the DOM
 window.htmlSectionsAdded = false;
+window.baseHtmlLoaded = false;
+
+// Array that will contain all of the sectionIDs that are to be
+// inserted into contentIndex
+let sectionIds = [];
+
+//Function to wait for the HTML sections to be added to the DOM (before starting js scripts / rendering React components)
+const waitForHtmlSectionsToInsertIntoDOM = () => {
+  return new Promise((resolve) => {
+    let interval = setInterval(() => {
+      let allPresentInDom = true;
+      for (const sectionId of sectionIds) {
+        if (!document.getElementById(sectionId)) {
+          allPresentInDom = false;
+          break;
+        }
+      }
+      if (allPresentInDom) {
+        clearInterval(interval);
+        resolve();
+      } else {
+        console.log("Nothing in DOM yet, waiting 100ms");
+      }
+    }, 100);
+  });
+};
+
+//Function to wait for the React rendered sections to be added to the DOM
+const waitForReactRenderedSectionsToInsertIntoDOM = async () => {
+  while (
+    [...document.querySelectorAll("[data-component-type]")].some(
+      (section) => section.innerHTML === ""
+    )
+  ) {
+    console.log("Waiting for react to render");
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+
+  console.log("All sections have been rendered");
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
   const links = document.querySelectorAll('link[rel="import"]');
   let contentIndex = document.querySelector("#content");
-
-  // Array that will contain all of the sectionIDs that are to be
-  // inserted into contentIndex
-  let sectionIds = [];
 
   // Import and add each page to the DOM
   for (const link of links) {
@@ -27,45 +66,14 @@ document.addEventListener("DOMContentLoaded", async function () {
     contentIndex.innerHTML += content;
   }
 
-  //Check to see if the links have been added to the DOM
-  //If not, try again in 100ms
-  const waitForHtmlSectionsToInsertIntoDOM = () => {
-    return new Promise((resolve) => {
-      let interval = setInterval(() => {
-        let allPresentInDom = true;
-        for (const sectionId of sectionIds) {
-          if (!document.getElementById(sectionId)) {
-            allPresentInDom = false;
-            break;
-          }
-        }
-        if (allPresentInDom) {
-          clearInterval(interval);
-          resolve();
-        } else {
-          console.log("Nothing in DOM yet, waiting 100ms");
-        }
-      }, 100);
-    });
-  };
+  // Wait for the HTML sections to be added to the DOM before rendering React components
+  await waitForHtmlSectionsToInsertIntoDOM();
   window.htmlSectionsAdded = true;
 
-  await waitForHtmlSectionsToInsertIntoDOM();
-  const waitForReactRenderedSectionsToInsertIntoDOM = async () => {
-    while (
-      [...document.querySelectorAll("[data-component-type]")].some(
-        (section) => section.innerHTML === ""
-      )
-    ) {
-      console.log("Waiting for react to render");
-      await new Promise((resolve) => setTimeout(resolve, 100));
-    }
-
-    console.log("All sections have been rendered");
-  };
-
+  // Wait for the React rendered sections to be added to the DOM
   await waitForReactRenderedSectionsToInsertIntoDOM();
   addDatasetAndOrganizationCardComponents();
 
+  // Set the base HTML loaded flag to true since all the HTML sections have been added to the DOM
   window.baseHtmlLoaded = true;
 });
