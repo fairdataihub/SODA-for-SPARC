0577ab5de873a301e25d81c8e0179dc6ed3d71ce
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-07-05 11:04:18 -0700

    wip: compare local remote UI - dummy data and loader working

--- src/renderer/src/scripts/advanced-features/compare-local-remote-dataset.js
+++ src/renderer/src/scripts/advanced-features/compare-local-remote-dataset.js
@@ -29,3 +29,64 @@ const questionTwoDatasetSelectionObserver = new MutationObserver(() => {
 questionTwoDatasetSelectionObserver.observe(document.querySelector("#compare-local-remote-dataset-ds-name"), {
     childList: true,
   });
+
+
+
+document.querySelector("#compare-local-remote-begin-comparison-btn").addEventListener("click", async () => {
+
+  // start the spinner 
+  document.querySelector("#comparing-local-remote-dataset-roller").classList.remove("hidden")
+
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  // get results
+  await compareLocalRemoteDataset();
+
+  // hide the spinner
+  document.querySelector("#comparing-local-remote-dataset-roller").classList.add("hidden")
+
+
+
+})
+
+
+const compareLocalRemoteDataset = async () => {
+    const localDatasetPath = document.querySelector("#compare-local-remote-dataset-local-path").value;
+    const remoteDatasetPath = window.defaultBfDatasetId; 
+
+    const comparisonResults = await getComparisonResults(localDatasetPath, remoteDatasetPath);
+
+    // check if there are any results 
+    if (comparisonResults.onlyLocal.length === 0 && comparisonResults.onlyPennsieve.length === 0) {
+        // no differences 
+        document.querySelector("#compare-local-remote-dataset-no-differences").style.display = "flex";
+        return;
+    }
+
+    populateFilePaths(document.querySelector("#comparison-results-only-on-pennsieve-table"), comparisonResults.onlyPennsieve);
+    populateFilePaths(document.querySelector("#comparison-results-only-on-local-table"), comparisonResults.onlyLocal);    
+}
+
+
+const populateFilePaths = (targetTableElement, differenceResultsList) => {
+  let tableBody = targetTableElement.getElementsByTagName("tbody")[0];
+  for (const filePath of differenceResultsList) {
+    let row = tableBody.insertRow(-1);
+    let cell = row.insertCell(0);
+    let newText = document.createTextNode(filePath);
+    // left align the text in the cell
+    cell.style.textAlign = "left";
+    cell.style.color = "black";
+    cell.appendChild(newText);
+  }
+};
+
+
+const getComparisonResults = async () => {
+  // TOOD: Make request to server 
+
+  return {
+    "onlyLocal": ["file/one", "file/two"],
+    "onlyPennsieve": ["file/three", "file/four"]
+  }
+}
