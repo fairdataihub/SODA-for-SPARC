03eb246ae867c452dedb993162d6842f10c2bbe5
jacobiclark <jacobcs@zoho.com>
2024-10-17 14:14:15 -0700

    wip: Added Dataset tree view renderer

---
+++ src/renderer/src/components/shared/DatasetTreeViewRenderer/index.jsx
@@ -0,0 +1,5 @@
+const DatasetTreeViewRenderer = ({ datasetStructure }) => {
+  return <div>{JSON.stringify(datasetStructure)}</div>;
+};
+
+export default DatasetTreeViewRenderer;
--- src/renderer/src/components/shared/manifest/ManifestEntitySelector.jsx
+++ src/renderer/src/components/shared/manifest/ManifestEntitySelector.jsx
@@ -1,14 +1,14 @@
 import { Grid, Button } from "@mantine/core";
 import FullWidthContainer from "../../containers/FullWidthContainer";
-import useGlobalStore from "../../../stores/globalStore"; // or wherever your store hook is
+import useGlobalStore from "../../../stores/globalStore";
+import DatasetTreeViewRenderer from "../DatasetTreeViewRenderer";
 
 const ManifestEntitySelector = () => {
   const setDatasetStructureJSONObj = useGlobalStore((state) => state.setDatasetStructureJSONObj);
   const datasetStructureJSONObj = useGlobalStore((state) => state.datasetStructureJSONObj);
 
   const handleButtonClick = () => {
-    console.log(window.datasetStructureJSONObj);
-    setDatasetStructureJSONObj({ hi: "test" });
+    setDatasetStructureJSONObj(window.datasetStructureJSONObj);
   };
 
   return (
@@ -16,7 +16,9 @@ const ManifestEntitySelector = () => {
       <Button onClick={handleButtonClick}>Set Name</Button>
       <Grid>
         <Grid.Col span={3}>Entity</Grid.Col>
-        <Grid.Col span={9}>{JSON.stringify(datasetStructureJSONObj)}</Grid.Col>
+        <Grid.Col span={9}>
+          <DatasetTreeViewRenderer datasetStructure={datasetStructureJSONObj} />
+        </Grid.Col>
       </Grid>
     </FullWidthContainer>
   );
