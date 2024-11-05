4f8b9de0f2db0f077578b6a95e0e84f73ec2f049
aaronm-2112 <aaron.marroquin96@gmail.com>
2024-11-05 12:32:47 -0800

    fix: remove faux internet check

--- src/renderer/src/scripts/check-firewall/checkFirewall.js
+++ src/renderer/src/scripts/check-firewall/checkFirewall.js
@@ -6,13 +6,6 @@ import axios from "axios";
  * Returns: true if the client is blocked by an external firewall, false otherwise.
  */
 export const clientBlockedByExternalFirewall = async (url) => {
-  // if the user doesnt have an internet connection they are not blocked by a firewall
-  // A simplification but we check for internet elsewhere so this is okay here.
-  let internetConnection = navigator.onLine;
-  if (!internetConnection) {
-    return false;
-  }
-
   // check that the client can make an api request to Pennsieve's public API
   //make an axios request to this public endpoint: https://api.pennsieve.io/discover/datasets
   //if the request fails, the client is blocked by an external firewall
@@ -25,6 +18,6 @@ export const clientBlockedByExternalFirewall = async (url) => {
 };
 
 export const blockedMessage =
-  "SODA is unable to reach Pennsieve. On rare occasions Pennsieve is not available for short periods of time. If you intend to upload to the Pennsieve platform please try again later. If this issue persists it is possible that your network is blocking access to Pennsieve from SODA. You may need to contact your network administrator for assistance.";
+  "SODA is unable to reach Pennsieve. Please check that you are connected to the internet. On rare occasions Pennsieve is not available for short periods of time. If you intend to upload to the Pennsieve platform please try again later. If this issue persists and you have confirmed internet connectivity then it is possible that your network is blocking access to Pennsieve from SODA. You may need to contact your network administrator for assistance.";
 export const hostFirewallMessage =
   "SODA is unable to communicate with its server.  If this issue persists it is possible that your network is blocking access. You may need to contact your network administrator for assistance.";
