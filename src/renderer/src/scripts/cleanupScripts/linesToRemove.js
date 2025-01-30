cfe186d72963f7bb57d413b6b0ee8b5bf46f738d
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-01-29 17:05:22 -0800

    wip: add code cleanup files

--- src/renderer/src/scripts/cleanupScripts/linesToRemove.js
+++ src/renderer/src/scripts/cleanupScripts/linesToRemove.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 // Read the ESLint output file
 const eslintOutput = JSON.parse(fs.readFileSync('../formatted-eslint-output.json', 'utf8'));
 
-const messages = ["'event' is not defined.", "'path' is not defined.", "'event' is defined but never used.", "'path' is defined but never used."];
+const messages = ["'event' is not defined.", "'path' is not defined.", "'event' is defined but never used.", "'path' is defined but never used.", "Empty block statement."];
 
 // Filter messages with the specific message
 const unusedEventMessages = eslintOutput.filter(result =>
   result.messages.some(message =>
-    message.message === "'path' is not defined."
+    message.message === "Empty block statement."
   )
 );
 
 // Extract the lines and columns to remove
 const targetLine = unusedEventMessages.flatMap(result =>
   result.messages
-    .filter(message => message.message === "'path' is not defined.")
+    .filter(message => message.message === "Empty block statement.")
     .map(message => ({
       filePath: result.filePath,
       line: message.line,
