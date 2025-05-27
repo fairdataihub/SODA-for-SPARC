6bedbb65725e9542dc7d1a8e6d331d28bdfe8083
JacobiClark <jacobcs@zoho.com>
2025-05-27 12:18:49 -0700

    wip: remove pennsieve page setting logic

--- src/renderer/src/components/utils/ui/InstructionsTowardsLeftContainer/index.jsx
+++ src/renderer/src/components/utils/ui/InstructionsTowardsLeftContainer/index.jsx
@@ -5,7 +5,7 @@ const InstructionsTowardsLeftContainer = ({ children }) => {
   return (
     <Paper p="xs" radius="md" bg="gray.0" withBorder={true}>
       <Group align="center" justify="flex-start" spacing="md" wrap="nowrap">
-        <IconArrowLeft size={40} color="var(--mantine-color-gray-6)" stroke={1.6} />
+        <IconArrowLeft size={40} color="var(--color-light-green)" stroke={1.6} />
         {children}
       </Group>
     </Paper>
