e37a813a6e399ce9b34e8e601e283e96db0372e5
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-07-15 15:46:42 -0700

    fix: update for path fix

--- src/pyflask/startup/minimumApiVersion.py
+++ src/pyflask/startup/minimumApiVersion.py
@@ -8,4 +8,4 @@ def get_api_version():
     Returns the version of the API
     """
 
-    return {"version": os.getenv("API_VERSION", "15.88.88-beta")}
+    return {"version": os.getenv("API_VERSION", "15.77.77-beta")}
