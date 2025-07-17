4b0110aa4a1f354a194e631c8d1222c00bd4260d
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-07-17 13:14:29 -0700

    fix: UD local manifest generation

--- src/renderer/src/components/shared/ManifestFilePreviewSection/utils.js
+++ src/renderer/src/components/shared/ManifestFilePreviewSection/utils.js
@@ -1,3 +1,5 @@
+import client from "../../../scripts/client";
+import { clientError } from "../../../scripts/others/http-error-handler/error-handler";
 export const handleOrganizeDsGenerateLocalManifestCopyButtonClick = async () => {
   // Step 1: Prompt the user to select a folder to save the dataset
   const savePath = await window.electron.ipcRenderer.invoke(
@@ -39,24 +41,20 @@ export const handleOrganizeDsGenerateLocalManifestCopyButtonClick = async () => 
   // Step 5: Generate the unique folder path for the manifest files
   const manifestFolderSavePath = generateManifestFolderSavePath();
 
-  // Step 6: Extract manifest file data from the sodaCopy object
-  const manifestFileData = window.sodaCopy["manifest-files"];
-
-  console.log("Manifest File Data:", manifestFileData);
-
-  const manifestJSON = window.processManifestInfo(
-    manifestFileData["headers"],
-    manifestFileData["data"]
-  );
-
-  const jsonManifest = JSON.stringify(manifestJSON);
-
   const manifestPath = window.path.join(manifestFolderSavePath, "manifest.xlsx");
 
   window.fs.mkdirSync(manifestFolderSavePath, { recursive: true });
 
-  // Create the manifest file
-  window.convertJSONToXlsx(JSON.parse(jsonManifest), manifestPath);
+  // TODO: Use sodaCopy once we have fully featured merge existing back again
+  try {
+    await client.post("/prepare_metadata/manifest", {
+      soda: window.sodaJSONObj,
+      path_to_manifest_file: manifestPath,
+      upload_boolean: false,
+    });
+  } catch (error) {
+    clientError(error);
+  }
 
   // Step 8: Display a success notification
   window.notyf.open({
