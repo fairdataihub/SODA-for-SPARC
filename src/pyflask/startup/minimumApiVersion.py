fabc6cdcd1bae7a764fb0339719be3f2690bd815
JacobiClark <jacobcs@zoho.com>
2025-08-04 15:34:51 -0700

    wip: remove coming soon

--- src/pyflask/startup/minimumApiVersion.py
+++ src/pyflask/startup/minimumApiVersion.py
@@ -8,4 +8,4 @@ def get_api_version():
     Returns the version of the API
     """
 
-    return {"version": os.getenv("API_VERSION", "16.0.0")}
+    return {"version": os.getenv("API_VERSION", "16.0.0-beta")}
