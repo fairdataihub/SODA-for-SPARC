e0068c1e19af25e0461e580c00231cab873a0fdb
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-08-06 16:03:54 -0700

    feat: get all datasets and handle them in front end

--- src/pyflask/manageDatasets/manage_datasets.py
+++ src/pyflask/manageDatasets/manage_datasets.py
@@ -326,23 +326,23 @@ def fetch_user_datasets(return_only_empty_datasets=False):
     sorted_bf_datasets = sorted(store, key=lambda k: k["name"].upper())
 
     # If filtering for empty datasets, fetch files/folders for each
-    if return_only_empty_datasets:
-        filtered_datasets = []
-        for ds in sorted_bf_datasets:
-            try:
-                r = requests.get(f"{PENNSIEVE_URL}/datasets/{ds['id']}/packages", headers=create_request_headers(get_access_token()))
-                r.raise_for_status()
-                packages_response = r.json()
-                dataset_packages = packages_response.get("packages", [])
-                if not dataset_packages:
-                    namespace_logger.info(f"Dataset {ds['id']} is empty, adding to filtered list")
-                    filtered_datasets.append(ds)
+    # if return_only_empty_datasets:
+    #     filtered_datasets = []
+    #     for ds in sorted_bf_datasets:
+    #         try:
+    #             r = requests.get(f"{PENNSIEVE_URL}/datasets/{ds['id']}/packages", headers=create_request_headers(get_access_token()))
+    #             r.raise_for_status()
+    #             packages_response = r.json()
+    #             dataset_packages = packages_response.get("packages", [])
+    #             if not dataset_packages:
+    #                 namespace_logger.info(f"Dataset {ds['id']} is empty, adding to filtered list")
+    #                 filtered_datasets.append(ds)
                 
-            except Exception as e:
-                namespace_logger.error(f"Error checking files for dataset {ds['id']}: {e}")
-        return {"datasets": filtered_datasets}
-    else:
-        return {"datasets": sorted_bf_datasets}
+    #         except Exception as e:
+    #             namespace_logger.error(f"Error checking files for dataset {ds['id']}: {e}")
+    #     return {"datasets": filtered_datasets}
+    # else:
+    return {"datasets": sorted_bf_datasets}
 
 def get_username(accountname):
     """
