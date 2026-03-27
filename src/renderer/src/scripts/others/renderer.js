28aa9427d2a189ad7dc12f02fffca439d6656842
aaronm-2112 <aaron.marroquin96@gmail.com>
2026-03-27 13:30:31 -0700

    chore: add timer

--- src/renderer/src/scripts/others/renderer.js
+++ src/renderer/src/scripts/others/renderer.js
@@ -6716,6 +6716,8 @@ window.directToFreeFormMode = async () => {
 
 const directToOrganize = document.getElementById("button-homepage-freeform-mode");
 directToOrganize.addEventListener("click", async () => {
+  // start a timer
+  let now = Date.now();
   let homePath = window.homeDirectory;
 
   let sodaJSONObj = {
@@ -6794,8 +6796,14 @@ directToOrganize.addEventListener("click", async () => {
     )
     .then((res) => {
       console.log("Done Running");
+      let stop = Date.now();
+      let timeElapsed = (stop - now) / 1000; // in seconds
+      console.log(timeElapsed);
     })
     .catch(async (e) => {
+      let stop = Date.now();
+      let timeElapsed = (stop - now) / 1000; // in seconds
+      console.log(timeElapsed);
       if (!e.response && e.request && e.isAxiosError) {
         await swalShowError(
           "Network Error",
@@ -6811,6 +6819,9 @@ directToOrganize.addEventListener("click", async () => {
         let data = await client.get("http://localhost:4242/curate_datasets/curation/progress");
         await new Promise((resolve) => setTimeout(resolve, 1000));
       } catch (e) {
+        let stop = Date.now();
+        let timeElapsed = (stop - now) / 1000; // in seconds
+        console.log(timeElapsed);
         if (!e.response && e.request && e.isAxiosError) {
           await swalShowError(
             "Network Error",
