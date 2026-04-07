2d675177d673698e4a0e8b8219a0dfea2cfce6c0
aaronm-2112 <aaron.marroquin96@gmail.com>
2026-04-07 08:19:59 -0700

    wip: add gunicorn server for upload

---
+++ src/uploadServer/app.py
@@ -0,0 +1,203 @@
+from flask import Flask
+import os
+import requests
+from pennsieve import Pennsieve
+import time
+from configparser import ConfigParser
+import boto3
+import threading
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+PENNSIEVE_URL = "https://api.pennsieve.io"
+userpath = os.path.expanduser("~")
+configpath = os.path.join(userpath, ".pennsieve", "config.ini")
+
+
+upload_subprocess = None
+ps = None
+thread = None
+session_manifest_id = None
+done = False
+session_timer = None
+session_dataset_id = None
+
+def monitor_subscriber_progress(events_dict):
+    """
+    Monitors the progress of a subscriber and unsubscribes once the upload finishes. 
+    """
+    global done
+    global session_timer
+    global ps
+
+    now = time.time()
+
+    elapsed_time = now - session_timer
+
+    if elapsed_time > 1800:
+        ps.unsubscribe(10)
+        ps = None
+        # app.logger.info("[SUBSCRIBER Ended]")
+
+    if events_dict["type"] == 1:  # upload status: file_id, total, current, worker_id
+        file_id = events_dict["upload_status"].file_id
+        total_bytes_to_upload = events_dict["upload_status"].total
+        current_bytes_uploaded = events_dict["upload_status"].current
+
+        status = events_dict["upload_status"].status
+        if status == "2" or status == 2:
+            done = True
+            ps.unsubscribe(10) 
+            ps = None
+            # app.logger.info("[UPLOAD COMPLETE EVENT RECEIVED]")
+
+def get_account_name():
+    config = ConfigParser()
+    config.read(configpath)
+    if "global" not in config:
+        raise Exception("Profile has not been set")
+
+    keyname = config["global"]["default_profile"]
+    return keyname
+
+
+def get_profile_name_from_api_key(key):
+    config = ConfigParser()
+    config.read(configpath)
+    if "global" not in config:
+        raise Exception("Profile has not been set")
+
+    keyname = config["global"]["default_profile"]
+
+    if keyname in config and key in config[keyname]:
+        return config[keyname][key]
+    return None
+
+def get_access_token(api_key=None, api_secret=None):
+    """
+        Creates a temporary access token for utilizing the Pennsieve API. Reads the api token and secret from the Pennsieve config.ini file.
+        get cognito config . If no target profile name is provided the default profile is used. 
+    """
+    
+    r = requests.get(f"{PENNSIEVE_URL}/authentication/cognito-config")
+    r.raise_for_status()
+
+    cognito_app_client_id = r.json()["tokenPool"]["appClientId"]
+    cognito_region_name = r.json()["region"]
+
+    cognito_idp_client = boto3.client(
+        "cognito-idp",
+        region_name=cognito_region_name,
+        aws_access_key_id="",
+        aws_secret_access_key="",
+    )
+
+    login_response = cognito_idp_client.initiate_auth(
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": get_profile_name_from_api_key("api_token"), "PASSWORD": get_profile_name_from_api_key("api_secret")},
+    ClientId=cognito_app_client_id,
+    )
+
+    cached_access_token = login_response["AuthenticationResult"]["AccessToken"]
+
+    return cached_access_token
+
+def connect_pennsieve_client(account_name):
+    """
+        Connects to Pennsieve Python client to the Agent and returns the initialized Pennsieve object.
+    """
+    return Pennsieve(profile_name=account_name)
+
+
+@app.route("/curation", methods=["POST"])
+def Curation():
+      global generate_start_time
+      global ps
+      global upload_subprocess
+      global thread
+      global session_manifest_id
+      global session_dataset_id
+      generate_start_time = time.time()
+
+      # data = request.get_json()
+
+      app.logger.info('/curation POST request')
+      app.logger.info('Getting account name')
+      account_name = get_account_name()
+      app.logger.info("Got account name")
+      app.logger.info("Connecting pennsieve client")
+      ps = connect_pennsieve_client(account_name)
+      app.logger.info("Pennsieve client connected")
+
+      app.logger.info("About to create dataset")
+      r = requests.post(f"{PENNSIEVE_URL}/datasets", headers={"Content-Type": "application/json", "Authorization": f"Bearer {get_access_token()}",}, json={"name": "400GB-test"})
+      r.raise_for_status()
+      app.logger.info("Dataset created")
+      ds_id = r.json()["content"]["id"]
+      ps.set_dataset(ds_id)
+      session_dataset_id = ds_id
+      main_curate_progress_message = ("Uploading data files...")
+      folder_path = os.path.join(os.path.expanduser("~"), "400GB-dataset")
+      md = ps.manifest.create(folder_path, "/")
+
+      ps = None
+
+      app.logger.info("Finished creating the upload manifest and returned from /curation/manifest")
+
+      return {
+          "main_curate_progress_message": "Finished",
+          "main_total_generate_dataset_size": "Bigly",
+          "main_curation_uploaded_files": 400,
+          "local_manifest_id": md.manifest_id,
+          "origin_manifest_id": "24",
+          "main_curation_total_files": 24456
+      }
+
+
+@app.route("/curation/subscribe", methods=["GET"])
+def curationSubscription():
+      global ps
+      global session_timer
+      global done
+      global session_dataset_id
+
+
+      account_name = get_account_name()
+      app.logger.info("Trying to connect to the Pennsieve client")
+      ps = connect_pennsieve_client(account_name)
+      app.logger.info("Connected to the Pennsieve client")
+      ps.set_dataset(session_dataset_id)
+
+      app.logger.info("Creating subscription session")
+      session_timer = time.time()
+      thread = threading.Thread(target=ps.subscribe, args=(10, False, monitor_subscriber_progress))
+      thread.start()
+
+      return {"session_complete": True, "done": done}
+
+
+
+@app.route("/curation/progress", methods=["GET"])
+def CurationProgress():
+  global generate_start_time
+  global main_curate_progress_message
+  global done
+  return {
+      "main_curate_status": "Generating that dataset",
+      "start_generate": 1,
+      "main_curate_progress_message": main_curate_progress_message,
+      "main_total_generate_dataset_size": 50000000,
+      "main_generated_dataset_size": 10000,
+      "elapsed_time_formatted": time.time() - generate_start_time,
+      "total_files_uploaded": 10,
+      "generated_dataset_id": "N:dataset:test",
+      "generated_dataset_int_id": 1234,
+      "curation_error_message": "Whoops forry folks",
+      "done": done
+  }
